28164040670798c89a7834b8f1289a84071f04d4
Dylan Fong <dylanfong.ut@gmail.com>
2022-09-15 19:55:32 -0400

    initial commit - setup webpack, typescript, gitignore, prettier, readme, etc.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cytoscape Web
+
+## Requirements
+
+- yarn 1.22.11
+- Node.js 16.8.0
+
+## Setup
+
+run `yarn install`
+
+## Commands
+
+- `build`: build the app for production
+- `dev`: run a dev server that watches code changes
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  // use content hash for cache busting
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  // watch the dist file for changes when using the dev server
+  devServer: {
+    static: './dist',
+  },
+  // generate html that points to the bundle with the updated hash
+  plugins: [new HtmlWebpackPlugin()],
+  // split bundle into two chunks, node modules(vendor code) in one bundle and app source code in the other
+  // when source code changes, only the source code bundle will need to be updated, not the vendor code
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+}
