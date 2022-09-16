b74da77482f21a6074b2a60d29f97253992d6cde
Dylan Fong <dylanfong.ut@gmail.com>
2022-09-16 15:09:28 -0400

    generate css files when running webpack instead of inline styles in html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.tsx',
@@ -15,7 +16,7 @@ module.exports = {
       // look for css files to transform into the bundle
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
@@ -34,6 +35,9 @@ module.exports = {
   },
   // generate html that points to the bundle with the updated hash
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
     new HtmlWebpackPlugin({
       template: './index.html',
     }),
