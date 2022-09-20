4ec7e40b93bb5f67145ac54bc326eeefd13483f6
Dylan Fong <dylanfong.ut@gmail.com>
2022-09-20 09:46:56 -0400

    test commit for netlify

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
+// import Cytoscape from 'cytoscape'
 import './index.css'
 import logo from './assets/react.svg'
 
+// let cy = Cytoscape({ headless: true })
+// console.log(cy)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div></div>
+    <div style={{ backgroundColor: 'black' }}></div>
     <img src={logo} className="logo react" alt="React logo" />
   </React.StrictMode>,
 )
