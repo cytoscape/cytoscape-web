ec426b88b58b486dbeb3befcbfac0a6bb8078da1
Dylan Fong <dylanfong.ut@gmail.com>
2022-10-13 13:59:50 -0400

    more data generation work

--- data/generateDB.ts
+++ data/generateDB.ts
@@ -1,30 +1,51 @@
 import { faker } from '@faker-js/faker'
 import { Workspace, CurrentNetwork, NetworkSummary } from '../src/models'
+import { Row } from '../src/models/Table'
+import { VisualStyle } from '../src/models/Style'
+import _ from 'lodash'
 
-// const createRandomRow = (key: IdType): Row => {
-//     return {
-//         key,
-
-//     }
-
-// }
+const emptyRow = (): Row => {
+  return {
+    key: '0',
+    data: {},
+  }
+}
 
 const createRandomNetworkSummary = (): NetworkSummary => {
   return {
     uuid: faker.datatype.uuid(),
     name: `${faker.hacker.noun()} network`,
     iconUrl: faker.image.abstract(),
-    attributes: { key: '0', data: {} },
+    attributes: emptyRow(),
     createdAt: faker.date.past().toString(),
     modifiedAt: faker.date.recent().toString(),
   }
 }
 
-const createRandomWorkspace = (): Workspace => {
+const createRandomWorkspace = (numNetworks: number): Workspace => {
+  const networkSummaries = _.times(numNetworks, () =>
+    createRandomNetworkSummary(),
+  )
   return {
     uuid: faker.datatype.uuid(),
     name: `${faker.name.fullName()}'s workspace ${faker.datatype.number()}`,
-    networkSummaries: [],
-    currentNetworkUUID: faker.datatype.uuid(),
+    networkSummaries,
+    currentNetworkUUID:
+      networkSummaries[faker.datatype.number(networkSummaries.length - 1)].uuid,
+  }
+}
+
+const createRandomVisualStyle = (): VisualStyle => {}
+
+const createRandomNetwork = (
+  networkSummary: NetworkSummary,
+): CurrentNetwork => {
+  return {
+    summary: networkSummary,
+    visualStyle: createRandomVisualStyle(),
+    networkView: createRandomNetworkView(),
+    edgeTable: emptyTable(),
+    nodeTable: emptyTable(),
+    network: createRandomNetworkModel(),
   }
 }
