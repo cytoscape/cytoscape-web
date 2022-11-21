e64825c182eeaf6a10bcc7fab928f83dcdea3bf7
Dylan Fong <dylanfong.ut@gmail.com>
2022-11-21 17:45:44 -0500

    convert node shape to constant obj map, create ui selector to change node shape

--- src/models/VisualStyleModel/VisualPropertyValue/NodeShapeType.ts
+++ src/models/VisualStyleModel/VisualPropertyValue/NodeShapeType.ts
@@ -1,10 +1,13 @@
-export type NodeShapeType =
-  | 'rectangle'
-  | 'diamond'
-  | 'ellipse'
-  | 'hexagon'
-  | 'octagon'
-  | 'parallelogram'
-  | 'round-rectangle'
-  | 'triangle'
-  | 'vee'
+export const NodeShapeType = {
+  Rectangle: 'rectangle',
+  Diamond: 'diamond',
+  Ellipse: 'ellipse',
+  Hexagon: 'hexagon',
+  Octagon: 'octagon',
+  Parallelogram: 'parallelogram',
+  RoundRectangle: 'round-rectangle',
+  Triangle: 'triangle',
+  Vee: 'vee',
+} as const
+
+export type NodeShapeType = typeof NodeShapeType[keyof typeof NodeShapeType]
