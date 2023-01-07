f57c9d104a3c6a1cb63253aec55c5683b0e23799
Dylan Fong <dylanfong.ut@gmail.com>
2023-01-06 23:48:27 -0500

    refine bypass form

--- src/components/Vizmapper/Forms/BypassForm.tsx
+++ src/components/Vizmapper/Forms/BypassForm.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Box, Popover, Typography } from '@mui/material'
+import { Box, Popover, Typography, Button } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 
 import { IdType } from '../../../models/IdType'
@@ -19,6 +19,10 @@ function BypassFormContent(props: {
   visualProperty: VisualProperty<VisualPropertyValueType>
 }): React.ReactElement {
   const { visualProperty, currentNetworkId } = props
+  const [bypassValue, setBypassValue] = React.useState(
+    visualProperty.defaultValue,
+  )
+
   const viewModels = useViewModelStore((state) => state.viewModels)
   const networkView = viewModels[currentNetworkId]
   const setBypass = useVisualStyleStore((state) => state.setBypass)
@@ -28,13 +32,22 @@ function BypassFormContent(props: {
   const table = tables[currentNetworkId]
   const nodeTable = table?.nodeTable
   const edgeTable = table?.edgeTable
+  const selectedNodes = Object.values(networkView?.nodeViews).filter(
+    (nodeView) => nodeView.selected,
+  )
+  const selectedEdges = Object.values(networkView?.edgeViews).filter(
+    (edgeView) => edgeView.selected,
+  )
+
+  const validElementsSelected =
+    (selectedNodes.length > 0 && visualProperty.group === 'node') ||
+    (selectedEdges.length > 0 && visualProperty.group === 'edge')
 
   return (
     <Box
       sx={{
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'space-between',
         width: '400px',
         height: '400px',
         minWidth: '30vw',
@@ -43,105 +56,119 @@ function BypassFormContent(props: {
       // make sure there is no hovered component when the mouse leaves the bypass form
       onMouseLeave={() => setHovered(props.currentNetworkId, null)}
     >
-      <Typography variant="body1">{`${visualProperty.displayName} bypasses`}</Typography>
+      <Typography
+        sx={{ m: 1 }}
+        variant="h6"
+      >{`${visualProperty.displayName} bypasses`}</Typography>
 
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          border: '1px solid gray',
+          p: 1,
           m: 1,
+          border: '1px solid gray',
+          maxHeight: '300px',
+          overflow: 'scroll',
         }}
       >
+        <Box>Create Bypasses</Box>
         <Box
           sx={{
-            p: 1,
-            m: 1,
-            border: '1px solid gray',
-            maxHeight: '300px',
-            overflow: 'scroll',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'start',
           }}
         >
-          <Box>Selected Elements</Box>
-          {visualProperty.group === 'node'
-            ? Object.values(networkView?.nodeViews)
-                .filter((nodeView) => nodeView.selected)
-                .map((nodeView) => {
-                  return <Box key={nodeView.id}>{`Node: ${nodeView.id}`}</Box>
+          <Box sx={{ width: '200px', border: '1px solid gray', m: 1 }}>
+            <Box>Selected Elements</Box>
+            {visualProperty.group === 'node' ? (
+              selectedNodes.length > 0 ? (
+                selectedNodes.map((nodeView) => {
+                  const eleTable =
+                    visualProperty.group === 'node' ? nodeTable : edgeTable
+                  const name = eleTable.rows.get(nodeView.id)?.name
+                  return (
+                    <Box
+                      onMouseEnter={() =>
+                        setHovered(props.currentNetworkId, nodeView.id)
+                      }
+                      onMouseLeave={() =>
+                        setHovered(props.currentNetworkId, null)
+                      }
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                      }}
+                      key={nodeView.id}
+                    >
+                      <Box
+                        sx={{ width: 50, m: 1 }}
+                      >{`${visualProperty.group}:  ${nodeView.id}`}</Box>
+                      <Box sx={{ width: 50, m: 1 }}>{name}</Box>
+                    </Box>
+                  )
                 })
-            : null}
-          {visualProperty.group === 'edge'
-            ? Object.values(networkView?.edgeViews)
-                .filter((edgeView) => edgeView.selected)
-                .map((edgeView) => {
-                  return <Box key={edgeView.id}>{`Edge: ${edgeView.id}`}</Box>
-                })
-            : null}
-        </Box>
-        <Box
-          sx={{
-            p: 1,
-            m: 1,
-            border: '1px solid gray',
-            maxHeight: '300px',
-            overflow: 'scroll',
-          }}
-        >
-          <Box>Current Bypasses</Box>
-          {Array.from(visualProperty?.bypassMap?.entries() ?? []).map(
-            ([eleId, value]) => {
-              const eleTable =
-                visualProperty.group === 'node' ? nodeTable : edgeTable
-              const name = eleTable.rows.get(eleId)?.name
-              return (
-                <Box
-                  onMouseEnter={() => setHovered(props.currentNetworkId, eleId)}
-                  onMouseLeave={() => setHovered(props.currentNetworkId, null)}
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                  }}
-                  key={eleId}
-                >
-                  <Box
-                    sx={{ width: 50, m: 1 }}
-                  >{`${visualProperty.group}:  ${eleId}`}</Box>
-                  <Box sx={{ width: 50, m: 1 }}>{name}</Box>
-                  <VisualPropertyValueForm
-                    visualProperty={visualProperty}
-                    currentValue={value}
-                    onValueChange={(value) => {
-                      setBypass(
-                        currentNetworkId,
-                        visualProperty.name,
-                        [eleId],
-                        value,
-                      )
-                    }}
-                  />
-                  <CloseIcon
-                    sx={{ '&:hover': { cursor: 'pointer' } }}
-                    onClick={() => {
-                      deleteBypass(currentNetworkId, visualProperty.name, [
-                        eleId,
-                      ])
-                    }}
-                  />
-                </Box>
+              ) : (
+                <Typography variant="caption">
+                  Select nodes to apply a style bypass to them
+                </Typography>
               )
-            },
-          )}
+            ) : null}
+            {visualProperty.group === 'edge' ? (
+              selectedEdges.length > 0 ? (
+                selectedEdges.map((edgeView) => {
+                  const eleTable =
+                    visualProperty.group === 'node' ? nodeTable : edgeTable
+                  const name = eleTable.rows.get(edgeView.id)?.name
+                  return (
+                    <Box
+                      onMouseEnter={() =>
+                        setHovered(props.currentNetworkId, edgeView.id)
+                      }
+                      onMouseLeave={() =>
+                        setHovered(props.currentNetworkId, null)
+                      }
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                      }}
+                      key={edgeView.id}
+                    >
+                      <Box
+                        sx={{ width: 50, m: 1 }}
+                      >{`${visualProperty.group}:  ${edgeView.id}`}</Box>
+                      <Box sx={{ width: 50, m: 1 }}>{name}</Box>
+                    </Box>
+                  )
+                })
+              ) : (
+                <Typography variant="caption">
+                  Select edges to apply a style bypass to them
+                </Typography>
+              )
+            ) : null}
+          </Box>
+          <Box sx={{ width: '200px', border: '1px solid gray', m: 1 }}>
+            <Box>Bypass Value</Box>
+            {validElementsSelected ? (
+              <Box>
+                <VisualPropertyValueForm
+                  visualProperty={visualProperty}
+                  currentValue={bypassValue}
+                  onValueChange={(
+                    newBypassValue: VisualPropertyValueType,
+                  ): void => setBypassValue(newBypassValue)}
+                />
+              </Box>
+            ) : null}
+          </Box>
         </Box>
-      </Box>
-      <Box sx={{ border: '1px solid gray', m: 1 }}>
-        <Box>Value Picker</Box>
-        <VisualPropertyValueForm
-          visualProperty={visualProperty}
-          currentValue={visualProperty.defaultValue}
-          onValueChange={(newBypassValue: VisualPropertyValueType): void => {
+        <Button
+          disabled={!validElementsSelected}
+          onClick={() => {
             let ids: IdType[] = []
             const nodeIds = Object.values(networkView?.nodeViews)
               .filter((nodeView) => nodeView.selected)
@@ -154,14 +181,66 @@ function BypassFormContent(props: {
             } else if (visualProperty.group === 'edge') {
               ids = edgeIds
             }
-            setBypass(
-              currentNetworkId,
-              visualProperty.name,
-              ids,
-              newBypassValue,
-            )
+            setBypass(currentNetworkId, visualProperty.name, ids, bypassValue)
           }}
-        />
+        >
+          Apply bypass to selected elements
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          p: 1,
+          m: 1,
+          border: '1px solid gray',
+          maxHeight: '300px',
+          overflow: 'scroll',
+        }}
+      >
+        <Box>Edit Bypasses</Box>
+        {Array.from(visualProperty?.bypassMap?.entries() ?? []).map(
+          ([eleId, value]) => {
+            const eleTable =
+              visualProperty.group === 'node' ? nodeTable : edgeTable
+            const name = eleTable.rows.get(eleId)?.name
+            return (
+              <Box
+                onMouseEnter={() => setHovered(props.currentNetworkId, eleId)}
+                onMouseLeave={() => setHovered(props.currentNetworkId, null)}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                }}
+                key={eleId}
+              >
+                <Box
+                  sx={{ width: 50, m: 1 }}
+                >{`${visualProperty.group}:  ${eleId}`}</Box>
+                <Box sx={{ width: 50, m: 1 }}>{name}</Box>
+                <VisualPropertyValueForm
+                  visualProperty={visualProperty}
+                  currentValue={value}
+                  onValueChange={(value) => {
+                    setBypass(
+                      currentNetworkId,
+                      visualProperty.name,
+                      [eleId],
+                      value,
+                    )
+                  }}
+                />
+                <CloseIcon
+                  sx={{ '&:hover': { cursor: 'pointer' } }}
+                  onClick={() => {
+                    deleteBypass(currentNetworkId, visualProperty.name, [eleId])
+                    setHovered(currentNetworkId, null)
+                  }}
+                />
+              </Box>
+            )
+          },
+        )}
       </Box>
     </Box>
   )
