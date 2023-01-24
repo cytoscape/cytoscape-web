b4de7b0a89cecdc216df076331c4d9c4fc56d9c8
Dylan Fong <dylanfong.ut@gmail.com>
2023-01-23 20:55:59 -0500

    add build script for netlify deploys

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.tsx'),
@@ -57,6 +58,13 @@ module.exports = {
     new ESLintPlugin({
       extensions: ['ts', 'tsx'],
     }),
+
+    // netlify requires a _redirects file in the root of the dist folder to work with react router
+    process.env.BUILD === 'netlify'
+      ? new CopyPlugin({
+          patterns: [{ from: 'netlify/_redirects', to: '.' }],
+        })
+      : null,
   ],
   // split bundle into two chunks, node modules(vendor code) in one bundle and app source code in the other
   // when source code changes, only the source code bundle will need to be updated, not the vendor code
