4ac8490988f75d2d1d7ae7e559a586c832052a4d
Dylan Fong <dylanfong.ut@gmail.com>
2023-03-17 09:25:41 -0400

    WIP UI changes for the network summary list

--- src/components/SummaryPanel/NetworkPropertyPanel.tsx
+++ src/components/SummaryPanel/NetworkPropertyPanel.tsx
@@ -1,17 +1,18 @@
 import { ReactElement, useState } from 'react'
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
+  Tooltip,
+  IconButton,
   Box,
   Theme,
   Typography,
+  Divider,
+  Paper,
+  Popover,
 } from '@mui/material'
 import { blueGrey } from '@mui/material/colors'
 import { useTheme } from '@mui/material/styles'
+import EditIcon from '@mui/icons-material/Edit'
 
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import { SummaryPanel } from './SummaryPanel'
 import { IdType } from '../../models/IdType'
 import { NdexNetworkSummary } from '../../models/NetworkSummaryModel'
 import { useWorkspaceStore } from '../../store/WorkspaceStore'
@@ -26,6 +27,22 @@ export const NetworkPropertyPanel = ({
   summary,
 }: NetworkPropertyPanelProps): ReactElement => {
   const theme: Theme = useTheme()
+
+  const [editNetworkSummaryAnchorEl, setEditNetworkSummaryAnchorEl] = useState<
+    HTMLButtonElement | undefined
+  >(undefined)
+
+  const showEditNetworkSummaryForm = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ): void => {
+    event.stopPropagation()
+    setEditNetworkSummaryAnchorEl(event.currentTarget)
+  }
+
+  const hideEditNetworkSummaryForm = (event: any): void => {
+    event.stopPropagation()
+    setEditNetworkSummaryAnchorEl(undefined)
+  }
 
   const currentNetworkId: IdType = useWorkspaceStore(
     (state) => state.workspace.currentNetworkId,
@@ -45,45 +62,65 @@ export const NetworkPropertyPanel = ({
     (state) => state.setCurrentNetworkId,
   )
 
-  const [expanded, setExpanded] = useState<boolean>(false)
-
-  const handleChange = (): void => {
-    setExpanded(!expanded)
-  }
-
   const backgroundColor: string =
     currentNetworkId === id ? blueGrey[100] : '#FFFFFF'
 
   return (
-    <Accordion
-      expanded={expanded}
-      onChange={handleChange}
-      onClick={() => {
-        setCurrentNetworkId(id)
-      }}
-    >
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="network-props-content"
-        id="network-props-header"
-        sx={{ backgroundColor, width: '100%' }}
+    <>
+      <Divider />
+      <Box
+        sx={{
+          backgroundColor,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          '&:hover': { cursor: 'pointer' },
+          p: 1,
+          pt: 2,
+          pb: 2,
+        }}
+        onClick={() => {
+          setCurrentNetworkId(id)
+        }}
       >
         <Box sx={{ width: '100%' }}>
-          <Typography variant={'body1'} sx={{ width: '100%' }}>
+          <Typography variant={'body2'} sx={{ width: '100%' }}>
             {summary.name}
           </Typography>
           <Typography
             variant={'subtitle2'}
             sx={{ width: '100%', color: theme.palette.text.secondary }}
           >
-            {`N: ${summary.nodeCount} (${selectedNodes.length}) / 
+            {`N: ${summary.nodeCount} (${selectedNodes.length}) /
           E: ${summary.edgeCount} (${selectedEdges.length})`}
           </Typography>
         </Box>
-      </AccordionSummary>
-      <AccordionDetails>
-        <SummaryPanel summary={summary} />
-      </AccordionDetails>
-    </Accordion>
+        <Tooltip title="Edit network properties">
+          <IconButton
+            size="small"
+            sx={{ width: 30, height: 30 }}
+            onClick={showEditNetworkSummaryForm}
+          >
+            <EditIcon sx={{ fontSize: 18 }} />
+          </IconButton>
+        </Tooltip>
+        <Popover
+          open={editNetworkSummaryAnchorEl !== undefined}
+          anchorEl={editNetworkSummaryAnchorEl}
+          onClose={hideEditNetworkSummaryForm}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          <Paper sx={{ p: 1 }}>
+            <Typography variant={'body2'}>{summary.name}</Typography>
+            <Typography variant={'body2'}>
+              {JSON.stringify(summary, null, 2)}
+            </Typography>
+          </Paper>
+        </Popover>
+      </Box>
+    </>
   )
 }
