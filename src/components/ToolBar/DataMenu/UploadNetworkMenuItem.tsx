321c69e997797f3ee470821e480f052fe33625a1
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2023-03-28 00:29:34 -0700

    update

--- src/components/ToolBar/DataMenu/UploadNetworkMenuItem.tsx
+++ src/components/ToolBar/DataMenu/UploadNetworkMenuItem.tsx
@@ -2,17 +2,67 @@ import { MenuItem } from '@mui/material'
 import { ReactElement } from 'react'
 import { BaseMenuProps } from '../BaseMenuProps'
 
+// @ts-expect-error-next-line
+import { CyNDEx } from '@js4cytoscape/ndex-client'
+
+import { useWorkspaceStore } from '../../../store/WorkspaceStore'
+import { useNetworkStore } from '../../../store/NetworkStore'
+import { useTableStore } from '../../../store/TableStore'
+import { useViewModelStore } from '../../../store/ViewModelStore'
+import { useVisualStyleStore } from '../../../store/VisualStyleStore'
+import { useNetworkSummaryStore } from '../../../store/NetworkSummaryStore'
+import { exportNetworkToCx2 } from '../../../store/exportCX'
+import { Network } from '../../../models/NetworkModel'
+
 export const UploadNetworkMenuItem = (
   props: BaseMenuProps,
 ): ReactElement => {
+  const cyndex = new CyNDEx();
+  const currentNetworkId = useWorkspaceStore(
+    (state) => state.workspace.currentNetworkId,
+  )
 
-  const handleUploadNetwork = (): void => {
+  const table = useTableStore((state) => state.tables[currentNetworkId])
+
+  const summary = useNetworkSummaryStore(
+    (state) => state.summaries[currentNetworkId],
+  )
+
+  const viewModel = useViewModelStore(
+    (state) => state.viewModels[currentNetworkId],
+  )
+  const visualStyle = useVisualStyleStore(
+    (state) => state.visualStyles[currentNetworkId],
+  )
+  const network = useNetworkStore((state) =>
+    state.networks.get(currentNetworkId),
+  ) as Network
+
+  const saveNetworkToFile = async (): Promise<void> => {
+    const cx = exportNetworkToCx2(
+      network,
+      visualStyle,
+      summary,
+      table.nodeTable,
+      table.edgeTable,
+      viewModel,
+      `Copy of ${summary.name}`,
+    )
+    console.log(cx);
+    cyndex.postCX2NetworkToCytoscape(cx);
     props.handleClose()
   }
 
-  return (
-    <MenuItem onClick={handleUploadNetwork}>
+  const handleUploadNetworkFromFile = async (): Promise<void> => {
+    await saveNetworkToFile()
+  }
+
+  const menuItem = (
+    <MenuItem
+      onClick={handleUploadNetworkFromFile}
+    >
       Upload Network from File
     </MenuItem>
   )
+    return <>{menuItem}</>
 }
