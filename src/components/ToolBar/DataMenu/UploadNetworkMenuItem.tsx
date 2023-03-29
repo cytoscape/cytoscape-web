5741bb2351d5dd08c755ac275e86ac9115128a64
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2023-03-28 21:40:59 -0700

    add uuid

--- src/components/ToolBar/DataMenu/UploadNetworkMenuItem.tsx
+++ src/components/ToolBar/DataMenu/UploadNetworkMenuItem.tsx
@@ -12,7 +12,24 @@ import { useViewModelStore } from '../../../store/ViewModelStore'
 import { useVisualStyleStore } from '../../../store/VisualStyleStore'
 import { useNetworkSummaryStore } from '../../../store/NetworkSummaryStore'
 import { exportNetworkToCx2 } from '../../../store/exportCX'
-import { Network } from '../../../models/NetworkModel'
+import NetworkFn, { Network } from '../../../models/NetworkModel'
+import { Cx2 } from '../../../utils/cx/Cx2'
+import VisualStyleFn, { VisualStyle } from '../../../models/VisualStyleModel'
+import ViewModelFn, { NetworkView } from '../../../models/ViewModel'
+import { v4 as uuidv4 } from 'uuid';
+
+import {
+  putNetworkToDb,
+  putVisualStyleToDb,
+  putNetworkViewToDb,
+} from '../../../store/persist/db'
+
+
+interface FullNetworkData {
+  network: Network
+  visualStyle: VisualStyle
+  networkView: NetworkView
+}
 
 export const UploadNetworkMenuItem = (
   props: BaseMenuProps,
@@ -37,6 +54,30 @@ export const UploadNetworkMenuItem = (
   const network = useNetworkStore((state) =>
     state.networks.get(currentNetworkId),
   ) as Network
+  
+  const addNetworkToWorkspace = useWorkspaceStore(
+    (state) => state.addNetworkIds,
+  )
+
+  const createDataFromLocalCx2 = async (
+    LocalNetworkId: string,
+    cxData: Cx2,
+  ): Promise<FullNetworkData> => {
+    const network: Network = NetworkFn.createNetworkFromCx(LocalNetworkId, cxData)
+    await putNetworkToDb(network)
+
+  
+    const visualStyle: VisualStyle = VisualStyleFn.createVisualStyleFromCx(cxData)
+    await putVisualStyleToDb(LocalNetworkId, visualStyle)
+  
+    const networkView: NetworkView = ViewModelFn.createViewModelFromCX(
+      LocalNetworkId,
+      cxData,
+    )
+    await putNetworkViewToDb(LocalNetworkId, networkView)
+  
+    return { network, visualStyle, networkView }
+  }
 
   const saveNetworkToFile = async (): Promise<void> => {
     const cx = exportNetworkToCx2(
@@ -50,6 +91,11 @@ export const UploadNetworkMenuItem = (
     )
     console.log(cx);
     cyndex.postCX2NetworkToCytoscape(cx);
+    const localUuid = uuidv4();
+    const res = await createDataFromLocalCx2(localUuid, cx)
+    console.log(localUuid)
+    console.log(res)
+    addNetworkToWorkspace(localUuid)
     props.handleClose()
   }
 
