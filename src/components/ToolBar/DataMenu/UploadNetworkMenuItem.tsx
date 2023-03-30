3e3b1595b2348a902e3ff0b75095380fee04de95
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2023-03-30 01:44:48 -0700

    update

--- src/components/ToolBar/DataMenu/UploadNetworkMenuItem.tsx
+++ src/components/ToolBar/DataMenu/UploadNetworkMenuItem.tsx
@@ -1,63 +1,48 @@
 import { MenuItem } from '@mui/material'
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import { BaseMenuProps } from '../BaseMenuProps'
-
-// @ts-expect-error-next-line
-import { CyNDEx } from '@js4cytoscape/ndex-client'
-
-import { useWorkspaceStore } from '../../../store/WorkspaceStore'
-import { useNetworkStore } from '../../../store/NetworkStore'
-import { useTableStore } from '../../../store/TableStore'
-import { useViewModelStore } from '../../../store/ViewModelStore'
-import { useVisualStyleStore } from '../../../store/VisualStyleStore'
-import { useNetworkSummaryStore } from '../../../store/NetworkSummaryStore'
-import { exportNetworkToCx2 } from '../../../store/exportCX'
-import NetworkFn, { Network } from '../../../models/NetworkModel'
 import { Cx2 } from '../../../utils/cx/Cx2'
+import NetworkFn, { Network } from '../../../models/NetworkModel'
 import VisualStyleFn, { VisualStyle } from '../../../models/VisualStyleModel'
 import ViewModelFn, { NetworkView } from '../../../models/ViewModel'
 import { v4 as uuidv4 } from 'uuid';
+import TableFn, { Table } from '../../../models/TableModel'
+import { useTableStore } from '../../../store/TableStore'
 
+import { useVisualStyleStore } from '../../../store/VisualStyleStore'
+import { useNetworkStore } from '../../../store/NetworkStore'
+import { useViewModelStore } from '../../../store/ViewModelStore'
 import {
   putNetworkToDb,
+  putTablesToDb,
   putVisualStyleToDb,
   putNetworkViewToDb,
 } from '../../../store/persist/db'
 
 
-interface FullNetworkData {
-  network: Network
-  visualStyle: VisualStyle
-  networkView: NetworkView
-}
-
 export const UploadNetworkMenuItem = (
   props: BaseMenuProps,
 ): ReactElement => {
-  const cyndex = new CyNDEx();
-  const currentNetworkId = useWorkspaceStore(
-    (state) => state.workspace.currentNetworkId,
-  )
+  interface JsonContent {
+    [key: string]: any;
+  }
 
-  const table = useTableStore((state) => state.tables[currentNetworkId])
+  interface FullNetworkData {
+    network: Network
+    nodeTable: Table
+    edgeTable: Table
+    visualStyle: VisualStyle
+    networkView: NetworkView
+  }
 
-  const summary = useNetworkSummaryStore(
-    (state) => state.summaries[currentNetworkId],
-  )
 
-  const viewModel = useViewModelStore(
-    (state) => state.viewModels[currentNetworkId],
-  )
-  const visualStyle = useVisualStyleStore(
-    (state) => state.visualStyles[currentNetworkId],
-  )
-  const network = useNetworkStore((state) =>
-    state.networks.get(currentNetworkId),
-  ) as Network
-  
-  const addNetworkToWorkspace = useWorkspaceStore(
-    (state) => state.addNetworkIds,
-  )
+  const addNewNetwork = useNetworkStore((state) => state.add)
+
+  const setVisualStyle = useVisualStyleStore((state) => state.set)
+
+  const setViewModel = useViewModelStore((state) => state.setViewModel)
+
+  const setTables = useTableStore((state) => state.setTables)
 
   const createDataFromLocalCx2 = async (
     LocalNetworkId: string,
@@ -66,7 +51,12 @@ export const UploadNetworkMenuItem = (
     const network: Network = NetworkFn.createNetworkFromCx(LocalNetworkId, cxData)
     await putNetworkToDb(network)
 
-  
+    const [nodeTable, edgeTable]: [Table, Table] = TableFn.createTablesFromCx(
+      LocalNetworkId,
+      cxData,
+    )
+    await putTablesToDb(LocalNetworkId, nodeTable, edgeTable)
+
     const visualStyle: VisualStyle = VisualStyleFn.createVisualStyleFromCx(cxData)
     await putVisualStyleToDb(LocalNetworkId, visualStyle)
   
@@ -76,39 +66,48 @@ export const UploadNetworkMenuItem = (
     )
     await putNetworkViewToDb(LocalNetworkId, networkView)
   
-    return { network, visualStyle, networkView }
+    return { network, nodeTable, edgeTable, visualStyle, networkView }
   }
+  
+    const [jsonContent, setJsonContent] = useState<JsonContent | null>(null);
+  
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+      const file = event.target.files?.[0];
+      if (file == null) return;
+      const reader = new FileReader();
+      reader.onload = async (event) => {
+        try {
+          const json = JSON.parse(event.target?.result as string);
+          setJsonContent(json);
+          console.log(json);
+          const localUuid = uuidv4();
+          const res = await createDataFromLocalCx2(localUuid, json)
+          console.log(res)
+          const { network, nodeTable, edgeTable, visualStyle, networkView } = res
 
-  const saveNetworkToFile = async (): Promise<void> => {
-    const cx = exportNetworkToCx2(
-      network,
-      visualStyle,
-      summary,
-      table.nodeTable,
-      table.edgeTable,
-      viewModel,
-      `Copy of ${summary.name}`,
-    )
-    console.log(cx);
-    cyndex.postCX2NetworkToCytoscape(cx);
-    const localUuid = uuidv4();
-    const res = await createDataFromLocalCx2(localUuid, cx)
-    console.log(localUuid)
-    console.log(res)
-    addNetworkToWorkspace(localUuid)
-    props.handleClose()
-  }
-
-  const handleUploadNetworkFromFile = async (): Promise<void> => {
-    await saveNetworkToFile()
-  }
+          addNewNetwork(network)
+          setVisualStyle(localUuid, visualStyle)
+          setTables(localUuid, nodeTable, edgeTable)
+          setViewModel(localUuid, networkView)
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      reader.readAsText(file);
+    };
+  
 
   const menuItem = (
-    <MenuItem
-      onClick={handleUploadNetworkFromFile}
-    >
-      Upload Network from File
-    </MenuItem>
+    <MenuItem component="label">
+    Upload network from file
+    <input
+      type="file"
+      accept=".cx2"
+      style={{ display: "none" }}
+      onChange={handleFileChange}
+    />
+  </MenuItem>
   )
-    return <>{menuItem}</>
+    return <>{menuItem}{(jsonContent != null) && <pre>{JSON.stringify(jsonContent, null, 2)}</pre>}</>
+
 }
