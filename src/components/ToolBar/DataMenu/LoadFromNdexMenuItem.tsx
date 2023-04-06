fd9064a39cb0fea15061941a6688d7abc1fdbc9a
Dylan Fong <dylanfong.ut@gmail.com>
2023-04-06 18:52:45 -0400

    restrict users from loading networks larger than the configured file size/element size from NDEx

--- src/components/ToolBar/DataMenu/LoadFromNdexMenuItem.tsx
+++ src/components/ToolBar/DataMenu/LoadFromNdexMenuItem.tsx
@@ -1,10 +1,13 @@
 import { MenuItem } from '@mui/material'
-import { ReactElement, useState } from 'react'
+import { ReactElement, useState, useContext } from 'react'
 import { IdType } from '../../../models/IdType'
 import { useWorkspaceStore } from '../../../store/WorkspaceStore'
 import { BaseMenuProps } from '../BaseMenuProps'
 import { LoadFromNdexDialog } from './LoadFromNdexDialog'
+import { networkSummaryFetcher } from '../../../store/useNdexNetworkSummary'
+import { useCredentialStore } from '../../../store/CredentialStore'
 
+import { AppConfigContext } from '../../../AppConfigContext'
 export const LoadFromNdexMenuItem = (props: BaseMenuProps): ReactElement => {
   const addNetworks: (ids: IdType | IdType[]) => void = useWorkspaceStore(
     (state) => state.addNetworkIds,
@@ -13,8 +16,10 @@ export const LoadFromNdexMenuItem = (props: BaseMenuProps): ReactElement => {
   const setCurrentNetworkId = useWorkspaceStore(
     (state) => state.setCurrentNetworkId,
   )
+  const getToken = useCredentialStore((state) => state.getToken)
 
   const [openDialog, setOpenDialog] = useState<boolean>(false)
+  const [invalidNetworkIds, setInvalidNetworkIds] = useState<IdType[]>([])
 
   const handleCloseDialog = (): void => {
     setOpenDialog(false)
@@ -24,12 +29,33 @@ export const LoadFromNdexMenuItem = (props: BaseMenuProps): ReactElement => {
   const handleOpenDialog = (): void => {
     setOpenDialog(true)
   }
+  const { ndexBaseUrl, maxNetworkElementsThreshold, maxNetworkFileSize } =
+    useContext(AppConfigContext)
 
-  const handleLoad = (uuidStr: string): void => {
+  const handleLoad = async (uuidStr: string): Promise<void> => {
     const uuids: IdType[] = uuidStr.split(' ')
+    const token = await getToken()
+    const summaries = await networkSummaryFetcher(uuids, ndexBaseUrl, token)
+
+    const invalidNetworkIds: IdType[] = []
+    const validNetworkIds: IdType[] = []
+
+    summaries.forEach((summary) => {
+      if (summary !== undefined) {
+        const networkSizeTooLarge = summary.cx2FileSize > maxNetworkFileSize
+        const tooManyNetworkElements =
+          summary.nodeCount + summary.edgeCount > maxNetworkElementsThreshold
+        if (networkSizeTooLarge || tooManyNetworkElements) {
+          invalidNetworkIds.push(summary.externalId)
+        } else {
+          validNetworkIds.push(summary.externalId)
+        }
+      }
+    })
     console.log('Given UUID string: ', uuidStr)
     console.log('Got UUID List: ', uuids)
-    addNetworks(uuids)
+    console.log('Valid networks', validNetworkIds)
+    addNetworks(validNetworkIds)
     let nextCurrentNetworkId: IdType | undefined
     if (Array.isArray(uuids)) {
       nextCurrentNetworkId = uuids[0]
@@ -40,8 +66,13 @@ export const LoadFromNdexMenuItem = (props: BaseMenuProps): ReactElement => {
     if (nextCurrentNetworkId !== undefined) {
       setCurrentNetworkId(nextCurrentNetworkId)
     }
-    setOpenDialog(false)
-    props.handleClose()
+
+    if (invalidNetworkIds.length > 0) {
+      setInvalidNetworkIds(invalidNetworkIds)
+    } else {
+      setOpenDialog(false)
+      props.handleClose()
+    }
   }
 
   return (
@@ -53,6 +84,7 @@ export const LoadFromNdexMenuItem = (props: BaseMenuProps): ReactElement => {
         open={openDialog}
         handleClose={handleCloseDialog}
         handleLoad={handleLoad}
+        invalidNetworkIds={invalidNetworkIds}
       />
     </>
   )
