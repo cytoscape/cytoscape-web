2ab79394d709f08ed7354edca337aff815c8dd92
Keiichiro Ono <keiono@gmail.com>
2023-08-01 00:54:31 -0700

    Updating shared style mechanism

--- src/features/HierarchyViewer/components/StyleSelector/StyleSelector.tsx
+++ src/features/HierarchyViewer/components/StyleSelector/StyleSelector.tsx
@@ -1,8 +1,9 @@
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useVisualStyleSelectorStore } from '../../store/VisualStyleSelectorStore'
+import { Button, FormHelperText, Stack } from '@mui/material'
 
 export const StyleSelector = (): JSX.Element => {
   const [selectedStyleName, setSelectedStyleName] = useState<string>('')
@@ -14,21 +15,42 @@ export const StyleSelector = (): JSX.Element => {
     setSelectedStyleName(event.target.value)
   }
 
+  useEffect(() => {
+    if (Object.keys(visualStyles).length > 0) {
+      setSelectedStyleName(Object.keys(visualStyles)[0])
+    }
+  }, [visualStyles])
+
+  const applyVisualStyle = (): void => {
+    console.log('applyVisualStyle')
+
+  }
+
   return (
-    <FormControl sx={{ width: '100%', minWidth: 120 }} size="small">
-      <Select
-        labelId="demo-select-small-label"
-        id="demo-select-small"
-        value={selectedStyleName}
-        label="Style"
-        onChange={handleChange}
-      >
-        {Object.keys(visualStyles).map((id: string) => (
-          <MenuItem key={id} value={id}>
-            {id}
+    <Stack direction="row" spacing={2}>
+      <FormControl sx={{ width: '100%', minWidth: 120 }} size="small">
+        <Select
+          labelId="demo-select-small-label"
+          id="demo-select-small"
+          value={selectedStyleName}
+          onChange={handleChange}
+        >
+          
+          {Object.keys(visualStyles).map((id: string) => (
+            <MenuItem key={id} value={id}>
+              {id}
+            </MenuItem>
+          ))}
+          <MenuItem value="preset1" key={'preset1'}>
+            Preset 1
           </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+          
+        </Select>
+        <FormHelperText>Shared Visual Style</FormHelperText>
+      </FormControl>
+      <Button variant="contained" size="small" sx={{ mt: 1 }} onClick={applyVisualStyle}>
+        Apply
+      </Button>
+    </Stack>
   )
 }
