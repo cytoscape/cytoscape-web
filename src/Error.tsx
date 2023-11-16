7d2a498245e8909b8e3438131ab138820da24717
Keiichiro Ono <keiono@gmail.com>
2023-11-16 14:45:23 -0800

    Confirmation dialog has been removed.

--- src/Error.tsx
+++ src/Error.tsx
@@ -1,35 +1,25 @@
 import { Button, Grid, Typography } from '@mui/material'
-import { ReactElement, useState } from 'react'
+import { ReactElement } from 'react'
 import {
   isRouteErrorResponse,
   useNavigate,
   useRouteError,
 } from 'react-router-dom'
-import { ConfirmationDialog } from './components/Util/ConfirmationDialog'
 import { useWorkspaceStore } from './store/WorkspaceStore'
 
 export const Error = (): ReactElement => {
   const error = useRouteError()
-  const [open, setOpen] = useState(false)
   const navigate = useNavigate()
   const resetWorkspace = useWorkspaceStore((state) => state.resetWorkspace)
 
   const handleReset = (): void => {
-    handleClose()
     resetWorkspace()
     navigate('/')
     navigate(0)
   }
 
-  const handleClickOpen = (): void => {
-    setOpen(true)
-  }
-
-  const handleClose = (): void => {
-    setOpen(false)
-  }
-  
   let status = 'Unknown'
+
   if (isRouteErrorResponse(error)) {
     const { statusText } = error
     status = statusText
@@ -57,22 +47,16 @@ export const Error = (): ReactElement => {
       </Grid>
       <Grid item xs={12}>
         <Typography variant="body1">
-          If this persists, you can reset your local workspace cache and
-          restart the app.
+          Your local cache might contain outdated 
+          workspace data and it can be the cause of this error. 
+          Please reset your local workspace cache and restart 
+          the app using the button below.
         </Typography>
       </Grid>
       <Grid item xs={12}>
-        <Button variant="outlined" color={'warning'} onClick={handleClickOpen}>
-          Reset and Reload Cytoscape
+        <Button variant="outlined" color={'warning'} onClick={handleReset}>
+          Reset Workspace and Reload Cytoscape
         </Button>
-        <ConfirmationDialog
-          title="Reset Local Workspace Cache"
-          message="Are you sure you want to reset all local workspace and restart the app? (This deletes all of the local cache)"
-          onConfirm={handleReset}
-          open={open}
-          setOpen={setOpen}
-          buttonTitle="Reset and Reload the Workspace (cannot be undone)"
-        />
       </Grid>
     </Grid>
   )
