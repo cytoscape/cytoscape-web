9d75fbe4416cc10a78bc0212cb808b2fe119ce57
Keiichiro Ono <keiono@gmail.com>
2023-12-05 22:22:06 -0800

    Trying to add global error state for wrong URL

--- src/RedirectPanel.tsx
+++ src/RedirectPanel.tsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useUiStateStore } from './store/UiStateStore'
 
 export const RedirectPanel = (): JSX.Element => {
   console.log('RED panel!!')
   const navigate = useNavigate()
   const [showError, setShowError] = useState<boolean>(true)
 
+  // Use global UI state
+  const setErrorMessage = useUiStateStore(
+    (state) => state.setErrorMessage,
+  )
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowError(false)
-    }, 2000)
+      setErrorMessage('Wrong URL')
+    }, 5000)
 
     // Clean up the timer when the component is unmounted
     return () => clearTimeout(timer)
   }, [])
 
-  if (!showError) {
-    navigate('/')
-  }
+  useEffect(() => {
+    if (!showError) {
+      navigate('/')
+    }
+  }, [showError])
 
   return (
     <div>
