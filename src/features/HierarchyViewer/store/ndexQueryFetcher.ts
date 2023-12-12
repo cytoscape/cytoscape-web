f3e418210afd8f1f4e0e0d2c4ef0f83ebd05bec6
Keiichiro Ono <keiono@gmail.com>
2023-12-12 15:41:17 -0800

    Modified fetcher force to re-fetch the data if the cache is not consistent.

--- src/features/HierarchyViewer/store/ndexQueryFetcher.ts
+++ src/features/HierarchyViewer/store/ndexQueryFetcher.ts
@@ -6,6 +6,9 @@ import {
 } from '../../../utils/cx-utils'
 import { CachedData } from '../../../utils/CachedData'
 import { getNdexClient } from '../../../utils/fetchers'
+import { NetworkView } from '../../../models/ViewModel'
+import { Network } from '../../../models/NetworkModel'
+import { IdType } from '../../../models/IdType'
 
 export const ndexQueryFetcher = async (
   params: string[],
@@ -47,37 +50,102 @@ export const ndexQueryFetcher = async (
       cache.visualStyle === undefined ||
       cache.networkView === undefined
     ) {
-      // Case 1: Simply fetch network if UUID is provided as node attribute
-      if (
-        interactionNetworkUuid !== undefined &&
-        interactionNetworkUuid !== ''
-      ) {
-        const cx2Network: Cx2 = await ndexClient.getCX2Network(
-          interactionNetworkUuid,
-        )
-        return await createDataFromCx(interactionNetworkId, cx2Network)
-      } else {
-        // Case 2: Just run the interconnect query if UUID is not provided
-        const cx2QueryResult: Cx2 = await ndexClient.interConnectQuery(
-          rootNetworkUuid,
-          null,
-          false,
-          query,
-          true,
-        )
-        return await createDataFromCx(interactionNetworkId, cx2QueryResult)
-      }
+      return await fetchFromRemote(
+        interactionNetworkId,
+        interactionNetworkUuid,
+        rootNetworkUuid,
+        query,
+        ndexClient,
+      )
     } else {
-      return {
-        network: cache.network,
-        nodeTable: cache.nodeTable,
-        edgeTable: cache.edgeTable,
-        visualStyle: cache.visualStyle,
-        networkView: cache.networkView,
+      const isValid = isValidNetworkAndView(cache.network, cache.networkView)
+
+      if (!isValid) {
+        return await fetchFromRemote(
+          interactionNetworkId,
+          interactionNetworkUuid,
+          rootNetworkUuid,
+          query,
+          ndexClient,
+        )
+      } else {
+        return {
+          network: cache.network,
+          nodeTable: cache.nodeTable,
+          edgeTable: cache.edgeTable,
+          visualStyle: cache.visualStyle,
+          networkView: cache.networkView,
+        }
       }
     }
   } catch (error) {
     console.error('Failed to get network', error)
     throw error
   }
+}
+
+const fetchFromRemote = async (
+  interactionNetworkId: IdType,
+  interactionNetworkUuid: IdType,
+  rootNetworkUuid: IdType,
+  query: string,
+  ndexClient: any,
+): Promise<NetworkWithView> => {
+  // Case 1: Simply fetch network if UUID is provided as node attribute
+  if (interactionNetworkUuid !== undefined && interactionNetworkUuid !== '') {
+    const cx2Network: Cx2 = await ndexClient.getCX2Network(
+      interactionNetworkUuid,
+    )
+    return await createDataFromCx(interactionNetworkId, cx2Network)
+  } else {
+    // Case 2: Just run the interconnect query if UUID is not provided
+    const cx2QueryResult: Cx2 = await ndexClient.interConnectQuery(
+      rootNetworkUuid,
+      null,
+      false,
+      query,
+      true,
+    )
+    return await createDataFromCx(interactionNetworkId, cx2QueryResult)
+  }
+}
+
+const isEqual = (set1: Set<any>, set2: Set<any>): boolean => {
+  if (set1.size !== set2.size) {
+    return false
+  }
+  for (const item of set1) {
+    if (!set2.has(item)) {
+      return false
+    }
+  }
+  return true
+}
+
+/**
+ * Return true if network and network view are consistent
+ *
+ * @param network
+ * @param networkView
+ * @returns
+ */
+const isValidNetworkAndView = (
+  network: Network,
+  networkView: NetworkView,
+): boolean => {
+  const nodeIdSet: Set<IdType> = new Set(network.nodes.map((node) => node.id))
+  const nodeViewIdSet: Set<IdType> = new Set(Object.keys(networkView.nodeViews))
+  const edgeIdSet: Set<IdType> = new Set(network.edges.map((edge) => edge.id))
+  const edgeViewIdSet: Set<IdType> = new Set(Object.keys(networkView.edgeViews))
+
+  if (!isEqual(nodeIdSet, nodeViewIdSet)) {
+    console.error('Network and network view are not consistent')
+    return false
+  }
+  if (!isEqual(edgeIdSet, edgeViewIdSet)) {
+    console.error('Network and network view are not consistent')
+    return false
+  }
+
+  return true
 }
