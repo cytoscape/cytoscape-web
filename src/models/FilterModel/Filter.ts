978f71999bf00d75ef7d8ffe93421d4a48adeae8
Keiichiro Ono <keiono@gmail.com>
2024-03-13 21:57:12 -0700

    Refactored API

--- src/models/FilterModel/Filter.ts
+++ src/models/FilterModel/Filter.ts
@@ -1,75 +1,43 @@
+import { stringify } from 'uuid'
+import { IdType } from '../IdType'
 import { GraphObjectType } from '../NetworkModel'
 import { DiscreteRange } from '../PropertyModel/DiscreteRange'
 import { NumberRange } from '../PropertyModel/NumberRange'
-import { ValueTypeName } from '../TableModel'
-
-/**
- * Type of filters
- * 
- * appliesTo: Specifies the widget's applicability to "nodes" or "edges".
- * 
-
-widgetType: Identifies the widget as either "checkbox" or "sliderbar".
-
-mappingSource (optional): A visual property name linked to the widget. Checkbox widgets should correspond with discrete mappings, while slider bars should align with continuous mappings.
-
-attributeName: The name of the attribute the widget filters on.
-label (optional): The displayed label for the checkbox group or slider bar. 
-tooltip (optional): Tooltip when a user mouse over the label. 
-definition: Defines the widget's behavior and configuration, which can be either:
-mappingTable:
-value: The attribute value.
-label: A human-readable label for the checkbox.
-tooltip: tooltip for the label. 
-range:
-min: The slider bar's minimum value.
-max: The maximum value of the slider bar.
-
- */
-
-/**
- * Type of filters
- */
-export const FilterType = {
-  DISCRETE: 'discrete',
-  CONTINUOUS: 'continuous',
-} as const
-
-export type FilterType = (typeof FilterType)[keyof typeof FilterType]
-
-export const FilterWidgetType = {
-  CHECKBOX: 'checkbox',
-  SLIDER: 'slider',
-} as const
-
-export type FilterWidgetType =
-  (typeof FilterWidgetType)[keyof typeof FilterWidgetType]
-
-/**
- * Definition and state of the filter UI
- */
-export interface FilterUI {
-  // Human-readable label for the filter
-  label: string
-  description?: string
-  widgetType: FilterWidgetType
-}
+import { AttributeName, Table, ValueType } from '../TableModel'
 
 /**
  * Base filter interface to be implemented by all filters
  */
-export interface Filter<T extends ValueTypeName> {
-  type: FilterType
-
-  // Human readable name of the filter
-  name: string
-
+interface FilterBase {
   target: GraphObjectType
-  attributeName: string
-  attributeType: T
-  readonly range?: NumberRange | DiscreteRange<T>
+  attribute: AttributeName
 }
 
-// interface FilterFunction<T extends ValueTypeName, V> {
-//   apply: (value: ValueType<T>) => V
-// }
+export interface DiscreteFilter<T> extends FilterBase {
+  apply: (range: DiscreteRange<T>, table: Table) => IdType[]
+}
+
+export interface NumericFilter extends FilterBase {
+  apply: (range: NumberRange, table: Table) => IdType[]
+}
+
+export const createNodeDiscreteFilter = <T>(
+  table: Table,
+  attribute: string,
+): DiscreteFilter<T> => {
+  return {
+    target: GraphObjectType.NODE,
+    attribute,
+    apply: (range: DiscreteRange<T>, table: Table): IdType[] => {
+      const rangeSet = new Set<T>(range.values)
+      const { rows } = table
+      const result: IdType[] = []
+      rows.forEach((row: Record<string, ValueType>) => {
+        if (rangeSet.has(row[attribute] as T)) {
+          result.push(row.id as IdType)
+        }
+      })
+      return result
+    },
+  }
+}
---
+++ src/models/FilterModel/FilterUi.ts
@@ -0,0 +1,40 @@
+import { DiscreteRange } from '../PropertyModel/DiscreteRange'
+import { NumberRange } from '../PropertyModel/NumberRange'
+import { ValueType } from '../TableModel'
+import { VisualMappingFunction } from '../VisualStyleModel'
+import { DiscreteFilter, NumericFilter } from './Filter'
+
+export const FilterWidgetType = {
+  CHECKBOX: 'checkbox',
+  SLIDER: 'slider',
+} as const
+
+export type FilterWidgetType =
+  (typeof FilterWidgetType)[keyof typeof FilterWidgetType]
+
+/**
+ * Interface for the filter user interface
+ */
+export interface FilterUi {
+  filter: DiscreteFilter<ValueType> | NumericFilter
+
+  // Human-readable label for the filter
+  label?: string
+
+  // More detailed description of the filter. Will be displayed in a tooltip
+  description?: string
+
+  // Type of the widget. Can be either "checkbox" or "slider"
+  widgetType: FilterWidgetType
+
+  // Visual mapping function that the filter is linked to. This is used to
+  // determine the visual representation of the filter. For example, if the
+  // filter is linked to a color visual mapping function, then the selectable
+  // options for the filter will be colored based on the visual mapping function.
+  visualMapping?: VisualMappingFunction
+
+  // Range of the values for the filter. If the widget type is "checkbox", then
+  // the range is a DiscreteRange. If the widget type is "slider", then the
+  // range is a NumberRange.
+  readonly range: NumberRange | DiscreteRange<ValueType>
+}
