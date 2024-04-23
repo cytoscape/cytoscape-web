5d85e6d51445bd6452bf60bbdb8083b2410da153
Keiichiro Ono <keiono@gmail.com>
2024-04-23 14:57:34 -0700

    Added a new flag to control the view depth

--- src/features/HierarchyViewer/components/CustomLayout/CirclePackingUtils.ts
+++ src/features/HierarchyViewer/components/CustomLayout/CirclePackingUtils.ts
@@ -1,5 +1,9 @@
 import * as d3Scale from 'd3-scale'
 import * as d3Interpolate from 'd3-interpolate'
+import * as d3Hierarchy from 'd3-hierarchy'
+import { VisualPropertyValueType } from '../../../../models/VisualStyleModel'
+import { CirclePackingView } from '../../model/CirclePackingView'
+import { NodeView } from '../../../../models/ViewModel'
 
 export const getColorMapper = (
   domain: [number, number],
@@ -9,4 +13,45 @@ export const getColorMapper = (
     .domain(domain)
     .range(['white', 'rgb(0,220,255)'])
     .interpolate(d3Interpolate.interpolateRgb)
+}
+
+export const getFontSize = (
+  d: d3Hierarchy.HierarchyCircularNode<any>,
+): number => {
+  return (d.r / 80) * 20
+}
+
+export const toCenter = (
+  svg: any,
+  dimensions: { width: number; height: number },
+): void => {
+  const centerX = dimensions.width / 2
+  const centerY = dimensions.height / 2
+  // Get the radius of the circle
+  const radius = svg.select('circle').attr('r')
+
+  const adjustedX = centerX - radius
+  const adjustedY = centerY - radius
+
+  svg.attr('transform', `translate(${adjustedX}, ${adjustedY})`)
+}
+
+export const getLabel = (
+  nodeId: string,
+  cpView: CirclePackingView,
+  defaultName: string,
+): string => {
+  let label: VisualPropertyValueType = ''
+  const nodeViews = cpView.nodeViews
+  if (nodeViews !== undefined) {
+    const nv: NodeView = nodeViews[nodeId]
+    if (nv !== undefined) {
+      label = nv.values.get('nodeLabel') as VisualPropertyValueType
+    }
+  }
+
+  if (label === undefined || label === '') {
+    return defaultName
+  }
+  return label.toString()
 }
