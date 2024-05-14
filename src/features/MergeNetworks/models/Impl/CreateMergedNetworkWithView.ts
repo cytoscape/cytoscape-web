1c8c93a5106013b7cea66ade51ba80913ae05ed1
Dylan Fong <dylanfong.ut@gmail.com>
2024-05-14 15:47:59 -0400

    fix ndex summary

--- src/features/MergeNetworks/models/Impl/CreateMergedNetworkWithView.ts
+++ src/features/MergeNetworks/models/Impl/CreateMergedNetworkWithView.ts
@@ -1,102 +1,117 @@
 import {
-    putNetworkToDb,
-    putTablesToDb,
-    putVisualStyleToDb,
-    putNetworkViewToDb,
-    putNetworkSummaryToDb,
+  putNetworkToDb,
+  putTablesToDb,
+  putVisualStyleToDb,
+  putNetworkViewToDb,
+  putNetworkSummaryToDb,
 } from '../../../../store/persist/db'
-import { NetworkWithView } from '../../../../utils/cx-utils';
-import { mergeNetwork } from './MergeNetwork';
-import { IdType } from '../../../../models/IdType';
-import { NetworkRecord, NetworktoMerge } from '../DataInterfaceForMerge';
-import VisualStyleFn, { VisualStyle } from '../../../../models/VisualStyleModel';
-import ViewModelFn, { NetworkView } from '../../../../models/ViewModel';
-import { deepClone } from '../../utils/helper-functions';
-import { NetworkAttributes } from '../../../../models/NetworkModel';
-import { Column } from '../../../../models/TableModel/Column';
-import { MatchingTable } from '../MatchingTable';
-import { getMatchingTableRows, getAttributeMapping } from './MatchingTableImpl';
-import { Visibility } from '../../../../models/NetworkSummaryModel/Visibility';
+import { NetworkWithView } from '../../../../utils/cx-utils'
+import { mergeNetwork } from './MergeNetwork'
+import { IdType } from '../../../../models/IdType'
+import { NetworkRecord, NetworktoMerge } from '../DataInterfaceForMerge'
+import VisualStyleFn, { VisualStyle } from '../../../../models/VisualStyleModel'
+import ViewModelFn, { NetworkView } from '../../../../models/ViewModel'
+import { deepClone } from '../../utils/helper-functions'
+import { NetworkAttributes } from '../../../../models/NetworkModel'
+import { Column } from '../../../../models/TableModel/Column'
+import { MatchingTable } from '../MatchingTable'
+import { getMatchingTableRows, getAttributeMapping } from './MatchingTableImpl'
+import { Visibility } from '../../../../models/NetworkSummaryModel/Visibility'
 
-export const createMergedNetworkWithView = async (fromNetworks: IdType[], toNetworkId: IdType, networkRecords: Record<IdType, NetworkRecord>,
-    nodeAttributeMapping: MatchingTable, edgeAttributeMapping: MatchingTable, networkAttributeMapping: MatchingTable,
-    matchingAttribute: Record<IdType, Column>, visualStyle: VisualStyle): Promise<NetworkWithView> => {
-    if (fromNetworks.length < 1) {
-        throw new Error("No networks to merge");
+export const createMergedNetworkWithView = async (
+  fromNetworks: IdType[],
+  toNetworkId: IdType,
+  networkRecords: Record<IdType, NetworkRecord>,
+  nodeAttributeMapping: MatchingTable,
+  edgeAttributeMapping: MatchingTable,
+  networkAttributeMapping: MatchingTable,
+  matchingAttribute: Record<IdType, Column>,
+  visualStyle: VisualStyle,
+): Promise<NetworkWithView> => {
+  if (fromNetworks.length < 1) {
+    throw new Error('No networks to merge')
+  }
+  if (getMatchingTableRows(nodeAttributeMapping).length < 2) {
+    throw new Error('Attributes Length should be greater than 1')
+  }
+  for (const netId of fromNetworks) {
+    if (!networkRecords[netId]) {
+      throw new Error(`Network with id ${netId} not found`)
     }
-    if (getMatchingTableRows(nodeAttributeMapping).length < 2) {
-        throw new Error("Attributes Length should be greater than 1")
+    if (!getAttributeMapping(nodeAttributeMapping, netId)) {
+      throw new Error(`Node attribute mapping for network ${netId} not found`)
     }
-    for (const netId of fromNetworks) {
-        if (!networkRecords[netId]) {
-            throw new Error(`Network with id ${netId} not found`);
-        }
-        if (!getAttributeMapping(nodeAttributeMapping, netId)) {
-            throw new Error(`Node attribute mapping for network ${netId} not found`);
-        }
-        if (!matchingAttribute[netId]) {
-            throw new Error(`Matching attribute for network ${netId} not found`);
-        }
+    if (!matchingAttribute[netId]) {
+      throw new Error(`Matching attribute for network ${netId} not found`)
     }
-    const newNetworkName = 'Merged Network'
-    const mergedNetwork: NetworkRecord = mergeNetwork(fromNetworks, toNetworkId, networkRecords,
-        nodeAttributeMapping, edgeAttributeMapping, networkAttributeMapping, matchingAttribute)
+  }
+  const newNetworkName = 'Merged Network'
+  const mergedNetwork: NetworkRecord = mergeNetwork(
+    fromNetworks,
+    toNetworkId,
+    networkRecords,
+    nodeAttributeMapping,
+    edgeAttributeMapping,
+    networkAttributeMapping,
+    matchingAttribute,
+  )
 
-    const baseNetSummary = 'Merged Network' //todo fecth from db
-    const newNetworkDescription = 'Merged Network'
-    // todo: merge network attributes also
-    const networkAttributes: NetworkAttributes = {
-        id: toNetworkId,
-        attributes: {},
-    }
+  const baseNetSummary = 'Merged Network' //todo fecth from db
+  const newNetworkDescription = 'Merged Network'
+  // todo: merge network attributes also
+  const networkAttributes: NetworkAttributes = {
+    id: toNetworkId,
+    attributes: {},
+  }
 
-    const newNetwork = mergedNetwork.network
-    const newNodeTable = mergedNetwork.nodeTable
-    const newEdgeTable = mergedNetwork.edgeTable
+  const newNetwork = mergedNetwork.network
+  const newNodeTable = mergedNetwork.nodeTable
+  const newEdgeTable = mergedNetwork.edgeTable
 
-    // Initialize new visual style and network view model
-    const newVisualStyle: VisualStyle = deepClone(visualStyle) || VisualStyleFn.createVisualStyle();
-    const newNetworkView: NetworkView = ViewModelFn.createViewModel(newNetwork)
+  // Initialize new visual style and network view model
+  const newVisualStyle: VisualStyle =
+    deepClone(visualStyle) || VisualStyleFn.createVisualStyle()
+  const newNetworkView: NetworkView = ViewModelFn.createViewModel(newNetwork)
 
-    await putNetworkSummaryToDb({
-        ownerUUID: toNetworkId,
-        name: newNetworkName,
-        isReadOnly: false,
-        subnetworkIds: [],
-        isValid: false,
-        warnings: [],
-        isShowcase: false,
-        isCertified: false,
-        indexLevel: '',
-        hasLayout: true,
-        hasSample: false,
-        cxFileSize: 0,
-        cx2FileSize: 0,
-        properties: [],
-        owner: '',
-        version: '',
-        completed: false,
-        visibility: 'PUBLIC' as Visibility,
-        nodeCount: newNetwork.nodes.length,
-        edgeCount: newNetwork.edges.length,
-        description: newNetworkDescription,
-        creationTime: new Date(Date.now()),
-        externalId: toNetworkId,
-        isDeleted: false,
-        modificationTime: new Date(Date.now()),
-    })
-    await putNetworkToDb(newNetwork);
-    await putTablesToDb(toNetworkId, newNodeTable, newEdgeTable);
-    await putVisualStyleToDb(toNetworkId, newVisualStyle)
-    await putNetworkViewToDb(toNetworkId, newNetworkView)
+  await putNetworkSummaryToDb({
+    isNdex: false,
+    ownerUUID: toNetworkId,
+    name: newNetworkName,
+    isReadOnly: false,
+    subnetworkIds: [],
+    isValid: false,
+    warnings: [],
+    isShowcase: false,
+    isCertified: false,
+    indexLevel: '',
+    hasLayout: true,
+    hasSample: false,
+    cxFileSize: 0,
+    cx2FileSize: 0,
+    properties: [],
+    owner: '',
+    version: '',
+    completed: false,
+    visibility: 'PUBLIC' as Visibility,
+    nodeCount: newNetwork.nodes.length,
+    edgeCount: newNetwork.edges.length,
+    description: newNetworkDescription,
+    creationTime: new Date(Date.now()),
+    externalId: toNetworkId,
+    isDeleted: false,
+    modificationTime: new Date(Date.now()),
+  })
+  await putNetworkToDb(newNetwork)
+  await putTablesToDb(toNetworkId, newNodeTable, newEdgeTable)
+  await putVisualStyleToDb(toNetworkId, newVisualStyle)
+  await putNetworkViewToDb(toNetworkId, newNetworkView)
 
-    return {
-        network: newNetwork,
-        nodeTable: newNodeTable,
-        edgeTable: newEdgeTable,
-        visualStyle: newVisualStyle,
-        networkViews: [newNetworkView],
-        networkAttributes,
-    }
+  return {
+    network: newNetwork,
+    nodeTable: newNodeTable,
+    edgeTable: newEdgeTable,
+    visualStyle: newVisualStyle,
+    networkViews: [newNetworkView],
+    networkAttributes,
+  }
 }
-
--- src/store/NetworkStore.ts
+++ src/store/NetworkStore.ts
@@ -62,176 +62,147 @@ interface NetworkActions {
 
 type NetworkStore = NetworkState & NetworkActions & UpdateActions
 
-const persist =
-  (config: StateCreator<NetworkStore>) =>
-  (
-    set: StoreApi<NetworkStore>['setState'],
-    get: StoreApi<NetworkStore>['getState'],
-    api: StoreApi<NetworkStore>,
-  ) =>
-    config(
-      async (args) => {
-        const currentNetworkId: IdType =
-          useWorkspaceStore.getState().workspace.currentNetworkId
-        set(args)
-        const updated: Network | undefined =
-          get().networks.get(currentNetworkId)
-        const deleted = updated === undefined
-        if (!deleted) {
-          console.debug('DB Update: network store', updated)
-          await putNetworkToDb(updated)
-        }
-      },
-      get,
-      api,
-    )
-
 export const useNetworkStore = create(
   subscribeWithSelector(
-    immer<NetworkStore>(
-      persist((set, get) => ({
-        networks: new Map<IdType, Network>(),
-        lastModified: undefined,
+    immer<NetworkStore>((set, get) => ({
+      networks: new Map<IdType, Network>(),
+      lastModified: undefined,
 
-        addNode: (networkId: IdType, nodeId: IdType) => {
-          set((state) => {
-            const network = state.networks.get(networkId)
-            if (network !== undefined) {
-              NetworkFn.addNode(network, nodeId)
-            }
+      addNode: (networkId: IdType, nodeId: IdType) => {
+        set((state) => {
+          const network = state.networks.get(networkId)
+          if (network !== undefined) {
+            NetworkFn.addNode(network, nodeId)
+          }
+          return state
+        })
+      },
+      addNodes: (networkId: IdType, nodeIds: IdType[]) => {
+        set((state) => {
+          const network = state.networks.get(networkId)
+          if (network !== undefined) {
+            NetworkFn.addNodes(network, nodeIds)
+          }
+          return {
+            networks: { ...state.networks },
+          }
+        })
+      },
+
+      deleteNodes: (networkId: IdType, nodeIds: IdType[]) => {
+        set((state) => {
+          if (nodeIds.length === 0) {
             return state
-          })
-        },
-        addNodes: (networkId: IdType, nodeIds: IdType[]) => {
-          set((state) => {
-            const network = state.networks.get(networkId)
-            if (network !== undefined) {
-              NetworkFn.addNodes(network, nodeIds)
+          }
+          const network = state.networks.get(networkId)
+          if (network !== undefined) {
+            const deletedElements = NetworkFn.deleteNodes(network, nodeIds)
+            const deletedNodes = deletedElements.nodes()
+            const deletedEdges = deletedElements.edges()
+            const deletedNodeIds = deletedNodes.map((node) => node.id())
+            const deletedEdgeIds = deletedEdges.map((edge) => edge.id())
+            const deleted = [...deletedNodeIds, ...deletedEdgeIds]
+            const event: NetworkUpdatedEvent = {
+              networkId,
+              type: UpdateEventType.DELETE,
+              payload: deleted,
             }
-            return {
-              networks: { ...state.networks },
-            }
-          })
-        },
-
-        deleteNodes: (networkId: IdType, nodeIds: IdType[]) => {
-          set((state) => {
-            if (nodeIds.length === 0) {
-              return state
-            }
-            const network = state.networks.get(networkId)
-            if (network !== undefined) {
-              const deletedElements = NetworkFn.deleteNodes(network, nodeIds)
-              const deletedNodes = deletedElements.nodes()
-              const deletedEdges = deletedElements.edges()
-              const deletedNodeIds = deletedNodes.map((node) => node.id())
-              const deletedEdgeIds = deletedEdges.map((edge) => edge.id())
-              const deleted = [...deletedNodeIds, ...deletedEdgeIds]
-              const event: NetworkUpdatedEvent = {
-                networkId,
-                type: UpdateEventType.DELETE,
-                payload: deleted,
-              }
-              state.lastUpdated = event
-            }
+            state.lastUpdated = event
+          }
+          return state
+        })
+      },
+      deleteEdges: (networkId: IdType, edgeIds: IdType[]) => {
+        set((state) => {
+          if (edgeIds.length === 0) {
             return state
-          })
-        },
-        deleteEdges: (networkId: IdType, edgeIds: IdType[]) => {
-          set((state) => {
-            if (edgeIds.length === 0) {
-              return state
-            }
+          }
 
-            const network = state.networks.get(networkId)
-            if (network !== undefined) {
-              const deletedElements = NetworkFn.deleteEdges(network, edgeIds)
+          const network = state.networks.get(networkId)
+          if (network !== undefined) {
+            const deletedElements = NetworkFn.deleteEdges(network, edgeIds)
 
-              const deletedEdges = deletedElements.edges()
-              const deletedEdgeIds = deletedEdges.map((edge) => edge.id())
-              const event: NetworkUpdatedEvent = {
-                networkId,
-                type: UpdateEventType.DELETE,
-                payload: deletedEdgeIds,
-              }
-              state.lastUpdated = event
+            const deletedEdges = deletedElements.edges()
+            const deletedEdgeIds = deletedEdges.map((edge) => edge.id())
+            const event: NetworkUpdatedEvent = {
+              networkId,
+              type: UpdateEventType.DELETE,
+              payload: deletedEdgeIds,
             }
+            state.lastUpdated = event
+          }
+          return state
+        })
+      },
+
+      addEdge: (networkId: IdType, id: IdType, s: IdType, t: IdType) => {
+        set((state) => {
+          const network = state.networks.get(networkId)
+          if (network !== undefined) {
+            NetworkFn.addEdge(network, { id, s, t })
+          }
+          return {
+            networks: { ...state.networks },
+          }
+        })
+      },
+      addEdges: (networkId: IdType, edges: Edge[]) => {
+        set((state) => {
+          const network = state.networks.get(networkId)
+          if (network !== undefined) {
+            NetworkFn.addEdges(network, edges)
+          }
+          return {
+            networks: { ...state.networks },
+          }
+        })
+      },
+
+      /**
+       *
+       * Add a new network to the store
+       *
+       * @param network new network to be added
+       * @returns
+       */
+      add: (network: Network) =>
+        set((state) => {
+          if (state.networks.has(network.id)) {
+            console.warn('Network already exists in store', network.id)
             return state
-          })
-        },
+          }
 
-        addEdge: (networkId: IdType, id: IdType, s: IdType, t: IdType) => {
-          set((state) => {
-            const network = state.networks.get(networkId)
-            if (network !== undefined) {
-              NetworkFn.addEdge(network, { id, s, t })
-            }
-            return {
-              networks: { ...state.networks },
-            }
-          })
-        },
-        addEdges: (networkId: IdType, edges: Edge[]) => {
-          set((state) => {
-            const network = state.networks.get(networkId)
-            if (network !== undefined) {
-              NetworkFn.addEdges(network, edges)
-            }
-            return {
-              networks: { ...state.networks },
-            }
-          })
-        },
-
-        /**
-         *
-         * Add a new network to the store
-         *
-         * @param network new network to be added
-         * @returns
-         */
-        add: (network: Network) =>
-          set((state) => {
-            if (state.networks.has(network.id)) {
-              console.warn('Network already exists in store', network.id)
-              return state
-            }
-
-            const newNetworkMap = new Map(state.networks).set(
-              network.id,
-              network,
-            )
-            state.networks = newNetworkMap
-            void putNetworkToDb(network)
-              .then(() => {
-                console.debug('* New network has been added to DB', network.id)
-              })
-              .catch((err) => {
-                console.error('Failed adding network to DB', err)
-              })
-            return state
-          }),
-        delete: (networkId: IdType) =>
-          set((state) => {
-            state.networks.delete(networkId)
-            void deleteNetworkFromDb(networkId).then(() => {
-              console.log('## Deleted network from db', networkId)
+          const newNetworkMap = new Map(state.networks).set(network.id, network)
+          state.networks = newNetworkMap
+          void putNetworkToDb(network)
+            .then(() => {
+              console.debug('* New network has been added to DB', network.id)
             })
-            return state
-          }),
-        deleteAll: () =>
-          set((state) => {
-            clearNetworksFromDb()
-              .then(() => {
-                console.log('Deleted all networks from db')
-              })
-              .catch((err) => {
-                console.warn('Error clearing all networks from db', err)
-              })
+            .catch((err) => {
+              console.error('Failed adding network to DB', err)
+            })
+          return state
+        }),
+      delete: (networkId: IdType) =>
+        set((state) => {
+          state.networks.delete(networkId)
+          void deleteNetworkFromDb(networkId).then(() => {
+            console.log('## Deleted network from db', networkId)
+          })
+          return state
+        }),
+      deleteAll: () =>
+        set((state) => {
+          clearNetworksFromDb()
+            .then(() => {
+              console.log('Deleted all networks from db')
+            })
+            .catch((err) => {
+              console.warn('Error clearing all networks from db', err)
+            })
 
-            return { ...state, networks: new Map<IdType, Network>() }
-          }),
-      })),
-    ),
+          return { ...state, networks: new Map<IdType, Network>() }
+        }),
+    })),
   ),
 )
