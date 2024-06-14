2645c818c2d34f4e5c8165c1437f834eace856da
Chengzhan Gao <1812535185@qq.com>
2024-06-14 15:33:30 -0700

    stick the workspace tab to the top

--- src/components/Workspace/NetworkBrowserPanel/NetworkBrowserPanel.tsx
+++ src/components/Workspace/NetworkBrowserPanel/NetworkBrowserPanel.tsx
@@ -141,11 +141,9 @@ export const NetworkBrowserPanel = ({
         {currentTabIndex === 0 && (
           <Box
             sx={{
-              overflow: 'auto',
-              // height: '100%',
-              // height: allotmentDimensions[0] - 48,
+              height: allotmentDimensions[0] - 96,
               // need to set a height to enable scroll in the network list
-              // 48 is the height of the tool bar
+              // 96 is the height of the tool bar
               width: '100%',
               padding: 0,
               margin: 0,
