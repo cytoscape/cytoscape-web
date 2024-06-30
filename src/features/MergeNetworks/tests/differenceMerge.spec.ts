7cfa51701721763476c2a8d5985485fa039bb31f
GAO Chengzhan <39005000+GAOChengzhan@users.noreply.github.com>
2024-06-29 22:47:55 -0700

    Clean Code

--- src/features/MergeNetworks/tests/differenceMerge.spec.ts
+++ src/features/MergeNetworks/tests/differenceMerge.spec.ts
@@ -19,7 +19,7 @@ import { Node } from '../../../models/NetworkModel/Node';
 import { Column } from '../../../models/TableModel/Column';
 import { Edge } from '../../../models/NetworkModel/Edge';
 import { createMatchingTable } from '../models/Impl/MatchingTableImpl';
-import { MatchingTableRow } from '../models/MatchingTable';
+import { MatchingTable, MatchingTableRow } from '../models/MatchingTable';
 import { differenceMerge } from '../models/Impl/DifferenceMerge';
 
 beforeEach(() => {
@@ -54,58 +54,53 @@ beforeEach(() => {
 });
 
 describe('intersection merge', () => {
-    it('should merge the networks successfully when only remove nodes if all their edges are being subtracted, too', () => {
-        const fromNetworks: IdType[] = ['net1', 'net2']
-        const toNetworkId = 'mergedNetwork'
-        const nodeLst1: Node[] = [{ id: 'n1' }, { id: 'n2' }, { id: 'n3' }, { id: 'n4' }, { id: 'n5' }, { id: 'n6' }]
-        const nodeLst2: Node[] = [{ id: 'n7' }, { id: 'n8' }, { id: 'n9' }, { id: 'n0' }]
+    let fromNetworks: IdType[];
+    let toNetworkId: IdType;
+    let nodeLst1: Node[];
+    let nodeLst2: Node[];
+    let edgeLst1: Edge[];
+    let edgeLst2: Edge[];
+    let net1: Network;
+    let net2: Network;
+    let nodeTableRows1: Map<IdType, any>;
+    let nodeTableRows2: Map<IdType, any>;
+    let edgeTableRows1: Map<IdType, any>;
+    let edgeTableRows2: Map<IdType, any>;
+    let nodeTable1: Table;
+    let nodeTable2: Table;
+    let edgeTable1: Table;
+    let edgeTable2: Table;
+    let networkRecords: Record<IdType, { network: Network, nodeTable: Table, edgeTable: Table }>;
+    let nodeAttributeMapping: MatchingTable;
+    let edgeAttributeMapping: MatchingTable;
+    let matchingAttribute: Record<IdType, Column>;
+
+    beforeEach(() => {
+        fromNetworks = ['net1', 'net2']
+        toNetworkId = 'mergedNetwork'
+        nodeLst1 = [{ id: 'n1' }, { id: 'n2' }, { id: 'n3' }, { id: 'n4' }, { id: 'n5' }, { id: 'n6' }]
+        nodeLst2 = [{ id: 'n7' }, { id: 'n8' }, { id: 'n9' }, { id: 'n0' }]
     
-        const edgeLst1: Edge[] = [{ id: 'e1', s: 'n1', t: 'n2' }, { id: 'e2', s: 'n2', t: 'n3' }, { id: 'e3', s: 'n3', t: 'n4' }, 
+        edgeLst1 = [{ id: 'e1', s: 'n1', t: 'n2' }, { id: 'e2', s: 'n2', t: 'n3' }, { id: 'e3', s: 'n3', t: 'n4' }, 
                                     { id: 'e4', s: 'n4', t: 'n1' },{ id: 'e5', s: 'n1', t: 'n3' }, { id: 'e6', s: 'n2', t: 'n4' },
                                     { id: 'e7', s: 'n4', t: 'n5' }, { id: 'e8', s: 'n5', t: 'n6' }]
-        const edgeLst2: Edge[] = [{ id: 'e1', s: 'n7', t: 'n8' }, { id: 'e2', s: 'n9', t: 'n8' }, { id: 'e3', s: 'n9', t: 'n0' },
+        edgeLst2 = [{ id: 'e1', s: 'n7', t: 'n8' }, { id: 'e2', s: 'n9', t: 'n8' }, { id: 'e3', s: 'n9', t: 'n0' },
                                     { id: 'e4', s: 'n0', t: 'n7' },{ id: 'e5', s: 'n7', t: 'n9' },{ id: 'e6', s: 'n8', t: 'n0' }]
-        const net1: Network = NetworkFn.createNetworkFromLists(fromNetworks[0], nodeLst1, edgeLst1)
-        const net2: Network = NetworkFn.createNetworkFromLists(fromNetworks[1], nodeLst2, edgeLst2)
+        net1 = NetworkFn.createNetworkFromLists(fromNetworks[0], nodeLst1, edgeLst1)
+        net2 = NetworkFn.createNetworkFromLists(fromNetworks[1], nodeLst2, edgeLst2)
     
-        const nodeTableRows1 = new Map();
-        nodeTableRows1.set(nodeLst1[0].id, { name: 'node 1'});
-        nodeTableRows1.set(nodeLst1[1].id, { name: 'node 2'});
-        nodeTableRows1.set(nodeLst1[2].id, { name: 'node 3'});
-        nodeTableRows1.set(nodeLst1[3].id, { name: 'node 4'});
-        nodeTableRows1.set(nodeLst1[4].id, { name: 'node 5'});
-        nodeTableRows1.set(nodeLst1[5].id, { name: 'node 6'});
+        nodeTableRows1 = new Map([...nodeLst1.entries()].map(([i, node]) => [node.id, { name: `node ${i + 1}` }]));
+        nodeTableRows2 = new Map([...nodeLst2.entries()].map(([i, node]) => [node.id, { name: `node ${i + 1}` }]));
+
+        edgeTableRows1 = new Map(edgeLst1.map(edge => [edge.id, { name: `edge ${edge.id?.slice(1)}`, interaction: 'a' } ] ));
+        edgeTableRows2 = new Map(edgeLst2.map(edge => [edge.id, { name: `edge ${edge.id?.slice(1)}`, interaction: edge.id === 'e6' ? 'b' : 'a' }]));
     
-        const nodeTableRows2 = new Map();
-        nodeTableRows2.set(nodeLst2[0].id, { name: 'node 1'});
-        nodeTableRows2.set(nodeLst2[1].id, { name: 'node 2' });
-        nodeTableRows2.set(nodeLst2[2].id, { name: 'node 3' });
-        nodeTableRows2.set(nodeLst2[3].id, { name: 'node 4' });
+        nodeTable1 = { id: fromNetworks[0], columns: [{ name: 'name', type: 'string' }], rows: nodeTableRows1 };
+        nodeTable2 = { id: fromNetworks[1], columns: [{ name: 'name', type: 'string' }], rows: nodeTableRows2 };
     
-        const edgeTableRows1 = new Map();
-        edgeTableRows1.set(edgeLst1[0].id, { name: 'edge 1', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[1].id, { name: 'edge 2', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[2].id, { name: 'edge 3', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[3].id, { name: 'edge 4', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[4].id, { name: 'edge 5', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[5].id, { name: 'edge 6', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[6].id, { name: 'edge 7', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[7].id, { name: 'edge 8', interaction: 'a' });
-    
-        const edgeTableRows2 = new Map();
-        edgeTableRows2.set(edgeLst2[0].id, { name: 'edge 1', interaction: 'a' });
-        edgeTableRows2.set(edgeLst2[1].id, { name: 'edge 2', interaction: 'a' });
-        edgeTableRows2.set(edgeLst2[2].id, { name: 'edge 3', interaction: 'a' });
-        edgeTableRows2.set(edgeLst2[3].id, { name: 'edge 4', interaction: 'a' });
-        edgeTableRows2.set(edgeLst2[4].id, { name: 'edge 5', interaction: 'a' });
-        edgeTableRows2.set(edgeLst2[5].id, { name: 'edge 6', interaction: 'b' });
-    
-        const nodeTable1 = { id: fromNetworks[0], columns: [{ name: 'name', type: 'string' }], rows: nodeTableRows1 };
-        const nodeTable2 = { id: fromNetworks[1], columns: [{ name: 'name', type: 'string' }], rows: nodeTableRows2 };
-    
-        const edgeTable1 = { id: fromNetworks[0], columns: [{ name: 'name', type: 'string' }, { name: 'interaction', type: 'string' }], rows: edgeTableRows1 };
-        const edgeTable2 = { id: fromNetworks[1], columns: [{ name: 'name', type: 'string' }, { name: 'interaction', type: 'string' }], rows: edgeTableRows2 };
-        const networkRecords = {
+        edgeTable1 = { id: fromNetworks[0], columns: [{ name: 'name', type: 'string' }, { name: 'interaction', type: 'string' }], rows: edgeTableRows1 };
+        edgeTable2 = { id: fromNetworks[1], columns: [{ name: 'name', type: 'string' }, { name: 'interaction', type: 'string' }], rows: edgeTableRows2 };
+        networkRecords = {
             [fromNetworks[0]]: {
                 network: net1,
                 nodeTable: nodeTable1 as Table,
@@ -117,7 +112,7 @@ describe('intersection merge', () => {
                 edgeTable: edgeTable2 as Table
             }
         }
-        const nodeAttributeMapping = createMatchingTable([
+        nodeAttributeMapping = createMatchingTable([
             {
                 id: 0, mergedNetwork: 'matchingAtt', type: 'string', hasConflicts: false,
                 typeRecord: { [fromNetworks[0]]: 'string', [fromNetworks[1]]: 'string', [fromNetworks[2]]: 'string' },
@@ -129,7 +124,7 @@ describe('intersection merge', () => {
                 nameRecord: { [fromNetworks[0]]: 'name', [fromNetworks[1]]: 'name', [fromNetworks[2]]: 'name' }
             }
         ] as MatchingTableRow[])
-        const edgeAttributeMapping = createMatchingTable([
+        edgeAttributeMapping = createMatchingTable([
             {
                 id: 0, mergedNetwork: 'name', type: 'string', hasConflicts: false,
                 typeRecord: { [fromNetworks[0]]: 'string', [fromNetworks[1]]: 'string', [fromNetworks[2]]: 'string' },
@@ -141,10 +136,12 @@ describe('intersection merge', () => {
                 nameRecord: { [fromNetworks[0]]: 'interaction', [fromNetworks[1]]: 'interaction', [fromNetworks[2]]: 'interaction' }
             }
         ] as MatchingTableRow[])
-        const matchingAttribute = {
+        matchingAttribute = {
             [fromNetworks[0]]: { name: 'name', type: 'string' } as Column,
             [fromNetworks[1]]: { name: 'name', type: 'string' } as Column
         }
+    });
+    it('should merge the networks successfully when remove all nodes that are in the second network', () => {
         const mergedNodeLst: Node[] = [{ id: '4' }, { id: '5' }]
         const mergedEdgeLst: Edge[] = [{ id: 'e0', s: '4', t: '5' }]
         const mergedNetwork: Network = NetworkFn.createNetworkFromLists(toNetworkId, mergedNodeLst, mergedEdgeLst)
@@ -163,97 +160,7 @@ describe('intersection merge', () => {
             edgeTable: mergedEdgeTable
         })
     });
-    it('should merge the networks successfully when remove all nodes that are in the second network', () => {
-        const fromNetworks: IdType[] = ['net1', 'net2']
-        const toNetworkId = 'mergedNetwork'
-        const nodeLst1: Node[] = [{ id: 'n1' }, { id: 'n2' }, { id: 'n3' }, { id: 'n4' }, { id: 'n5' }, { id: 'n6' }]
-        const nodeLst2: Node[] = [{ id: 'n7' }, { id: 'n8' }, { id: 'n9' }, { id: 'n0' }]
-    
-        const edgeLst1: Edge[] = [{ id: 'e1', s: 'n1', t: 'n2' }, { id: 'e2', s: 'n2', t: 'n3' }, { id: 'e3', s: 'n3', t: 'n4' }, 
-                                    { id: 'e4', s: 'n4', t: 'n1' },{ id: 'e5', s: 'n1', t: 'n3' }, { id: 'e6', s: 'n2', t: 'n4' },
-                                    { id: 'e7', s: 'n4', t: 'n5' }, { id: 'e8', s: 'n5', t: 'n6' }]
-        const edgeLst2: Edge[] = [{ id: 'e1', s: 'n7', t: 'n8' }, { id: 'e2', s: 'n9', t: 'n8' }, { id: 'e3', s: 'n9', t: 'n0' },
-                                    { id: 'e4', s: 'n0', t: 'n7' },{ id: 'e5', s: 'n7', t: 'n9' },{ id: 'e6', s: 'n8', t: 'n0' }]
-        const net1: Network = NetworkFn.createNetworkFromLists(fromNetworks[0], nodeLst1, edgeLst1)
-        const net2: Network = NetworkFn.createNetworkFromLists(fromNetworks[1], nodeLst2, edgeLst2)
-    
-        const nodeTableRows1 = new Map();
-        nodeTableRows1.set(nodeLst1[0].id, { name: 'node 1'});
-        nodeTableRows1.set(nodeLst1[1].id, { name: 'node 2'});
-        nodeTableRows1.set(nodeLst1[2].id, { name: 'node 3'});
-        nodeTableRows1.set(nodeLst1[3].id, { name: 'node 4'});
-        nodeTableRows1.set(nodeLst1[4].id, { name: 'node 5'});
-        nodeTableRows1.set(nodeLst1[5].id, { name: 'node 6'});
-    
-        const nodeTableRows2 = new Map();
-        nodeTableRows2.set(nodeLst2[0].id, { name: 'node 1'});
-        nodeTableRows2.set(nodeLst2[1].id, { name: 'node 2' });
-        nodeTableRows2.set(nodeLst2[2].id, { name: 'node 3' });
-        nodeTableRows2.set(nodeLst2[3].id, { name: 'node 4' });
-    
-        const edgeTableRows1 = new Map();
-        edgeTableRows1.set(edgeLst1[0].id, { name: 'edge 1', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[1].id, { name: 'edge 2', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[2].id, { name: 'edge 3', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[3].id, { name: 'edge 4', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[4].id, { name: 'edge 5', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[5].id, { name: 'edge 6', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[6].id, { name: 'edge 7', interaction: 'a' });
-        edgeTableRows1.set(edgeLst1[7].id, { name: 'edge 8', interaction: 'a' });
-    
-        const edgeTableRows2 = new Map();
-        edgeTableRows2.set(edgeLst2[0].id, { name: 'edge 1', interaction: 'a' });
-        edgeTableRows2.set(edgeLst2[1].id, { name: 'edge 2', interaction: 'a' });
-        edgeTableRows2.set(edgeLst2[2].id, { name: 'edge 3', interaction: 'a' });
-        edgeTableRows2.set(edgeLst2[3].id, { name: 'edge 4', interaction: 'a' });
-        edgeTableRows2.set(edgeLst2[4].id, { name: 'edge 5', interaction: 'a' });
-        edgeTableRows2.set(edgeLst2[5].id, { name: 'edge 6', interaction: 'b' });
-    
-        const nodeTable1 = { id: fromNetworks[0], columns: [{ name: 'name', type: 'string' }], rows: nodeTableRows1 };
-        const nodeTable2 = { id: fromNetworks[1], columns: [{ name: 'name', type: 'string' }], rows: nodeTableRows2 };
-    
-        const edgeTable1 = { id: fromNetworks[0], columns: [{ name: 'name', type: 'string' }, { name: 'interaction', type: 'string' }], rows: edgeTableRows1 };
-        const edgeTable2 = { id: fromNetworks[1], columns: [{ name: 'name', type: 'string' }, { name: 'interaction', type: 'string' }], rows: edgeTableRows2 };
-        const networkRecords = {
-            [fromNetworks[0]]: {
-                network: net1,
-                nodeTable: nodeTable1 as Table,
-                edgeTable: edgeTable1 as Table
-            },
-            [fromNetworks[1]]: {
-                network: net2,
-                nodeTable: nodeTable2 as Table,
-                edgeTable: edgeTable2 as Table
-            }
-        }
-        const nodeAttributeMapping = createMatchingTable([
-            {
-                id: 0, mergedNetwork: 'matchingAtt', type: 'string', hasConflicts: false,
-                typeRecord: { [fromNetworks[0]]: 'string', [fromNetworks[1]]: 'string', [fromNetworks[2]]: 'string' },
-                nameRecord: { [fromNetworks[0]]: 'name', [fromNetworks[1]]: 'name', [fromNetworks[2]]: 'name' }
-            },
-            {
-                id: 1, mergedNetwork: 'name', type: 'string', hasConflicts: false,
-                typeRecord: { [fromNetworks[0]]: 'string', [fromNetworks[1]]: 'string', [fromNetworks[2]]: 'string' },
-                nameRecord: { [fromNetworks[0]]: 'name', [fromNetworks[1]]: 'name', [fromNetworks[2]]: 'name' }
-            }
-        ] as MatchingTableRow[])
-        const edgeAttributeMapping = createMatchingTable([
-            {
-                id: 0, mergedNetwork: 'name', type: 'string', hasConflicts: false,
-                typeRecord: { [fromNetworks[0]]: 'string', [fromNetworks[1]]: 'string', [fromNetworks[2]]: 'string' },
-                nameRecord: { [fromNetworks[0]]: 'name', [fromNetworks[1]]: 'name', [fromNetworks[2]]: 'name' }
-            },
-            {
-                id: 1, mergedNetwork: 'interaction', type: 'string', hasConflicts: false,
-                typeRecord: { [fromNetworks[0]]: 'string', [fromNetworks[1]]: 'string', [fromNetworks[2]]: 'string' },
-                nameRecord: { [fromNetworks[0]]: 'interaction', [fromNetworks[1]]: 'interaction', [fromNetworks[2]]: 'interaction' }
-            }
-        ] as MatchingTableRow[])
-        const matchingAttribute = {
-            [fromNetworks[0]]: { name: 'name', type: 'string' } as Column,
-            [fromNetworks[1]]: { name: 'name', type: 'string' } as Column
-        }
+    it('should merge the networks successfully when only remove nodes if all their edges are being subtracted, too', () => {
         const mergedNodeLst: Node[] = [{ id: '4' }, { id: '5' }, { id: '1' }, {id:'2'}, {id:'3'}]
         const mergedEdgeLst: Edge[] = [{ id: 'e0', s: '1', t: '2' }, { id: 'e1', s: '1', t: '3' }, { id: 'e2', s: '3', t: '4' },
                                         { id: 'e3', s: '4', t: '5' }]
