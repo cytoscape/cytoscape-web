66ff2f0781eabb760b98c090b49b81e27c1740c4
Chengzhan Gao <1812535185@qq.com>
2024-08-26 23:16:08 -0700

    Node/Edge Naming in the bypass form

--- src/components/Vizmapper/Forms/BypassForm.tsx
+++ src/components/Vizmapper/Forms/BypassForm.tsx
@@ -17,11 +17,16 @@ import {
   TableContainer,
 } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
-
+import * as MapperFactory from '../../../models/VisualStyleModel/impl/MapperFactory'
 import { IdType } from '../../../models/IdType'
 import {
+  ContinuousMappingFunction,
+  DiscreteMappingFunction,
   EdgeVisualPropertyName,
+  Mapper,
+  MappingFunctionType,
   NodeVisualPropertyName,
+  PassthroughMappingFunction,
   VisualProperty,
   VisualPropertyValueType,
 } from '../../../models/VisualStyleModel'
@@ -58,7 +63,6 @@ function BypassFormContent(props: {
   const isSize =
     vpName === NodeVisualPropertyName.NodeHeight ||
     vpName === NodeVisualPropertyName.NodeWidth
-  const isHeight = vpName === NodeVisualPropertyName.NodeHeight
   const isEdgeLineColor =
     vpName === EdgeVisualPropertyName.EdgeLineColor ||
     vpName === EdgeVisualPropertyName.EdgeTargetArrowColor ||
@@ -66,12 +70,18 @@ function BypassFormContent(props: {
   const getViewModel = useViewModelStore((state) => state.getViewModel)
   const networkView: NetworkView | undefined = getViewModel(currentNetworkId)
 
+  const visualStyle = useVisualStyleStore((state) => state.visualStyles)
   const setBypass = useVisualStyleStore((state) => state.setBypass)
   const deleteBypass = useVisualStyleStore((state) => state.deleteBypass)
   const toggleSelected = useViewModelStore((state) => state.toggleSelected)
   const additiveSelect = useViewModelStore((state) => state.additiveSelect)
   const additiveUnselect = useViewModelStore((state) => state.additiveUnselect)
 
+  const labelName =
+    visualProperty.group === VisualPropertyGroup.Node
+      ? NodeVisualPropertyName.NodeLabel
+      : EdgeVisualPropertyName.EdgeLabel
+  const labelVp = visualStyle[currentNetworkId][labelName]
   const tables = useTableStore((state) => state.tables)
   const table = tables[currentNetworkId]
   const nodeTable = table?.nodeTable
@@ -111,11 +121,42 @@ function BypassFormContent(props: {
   let selectedElementsWithBypass = 0
   selectedElements.forEach((id: IdType) => {
     const hasBypass = visualProperty?.bypassMap.has(id)
+    const { defaultValue, mapping, bypassMap } = labelVp
+    // default name is the name attribute(if it exists) or the id
+    let name = selectedElementTable.rows.get(id)?.name ?? (id as string)
+    // if the mapping is defined, then overwrite with the mapped value
+    // with the priority of bypassMap > mapping > defaultValue
+    if (bypassMap !== undefined && bypassMap.has(id)) {
+      name = bypassMap.get(id) as string
+    } else if (mapping !== undefined) {
+      let mapper: Mapper
+      const mappingType: MappingFunctionType = mapping.type
+      if (mappingType === MappingFunctionType.Discrete) {
+        mapper = MapperFactory.createDiscreteMapper(
+          mapping as DiscreteMappingFunction,
+        )
+      } else if (mappingType === MappingFunctionType.Continuous) {
+        mapper = MapperFactory.createContinuousMapper(
+          mapping as ContinuousMappingFunction,
+        )
+      } else if (mappingType === MappingFunctionType.Passthrough) {
+        mapper = MapperFactory.createPassthroughMapper(
+          mapping as PassthroughMappingFunction,
+        )
+      } else {
+        throw new Error(`Unknown mapping type for ${vpName}`)
+      }
+      name = mapper(
+        selectedElementTable.rows.get(id)?.[mapping.attribute] ?? '',
+      ) as string
+    } else if (defaultValue !== undefined && defaultValue !== '') {
+      name = defaultValue as string
+    }
+
     elementsToRender.push({
       id,
       selected: true,
-      name: (selectedElementTable.rows.get(id)?.name ?? '') as string,
-
+      name: name as string,
       hasBypass: hasBypass ?? false,
     })
 
