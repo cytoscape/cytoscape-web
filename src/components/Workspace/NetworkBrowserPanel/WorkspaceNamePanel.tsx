bc1089c326b2ba9c24533da1808f8003f937a008
Keiichiro Ono <keiono@gmail.com>
2024-09-05 17:06:07 -0700

    Workspace name editor updated

--- src/components/Workspace/NetworkBrowserPanel/WorkspaceNamePanel.tsx
+++ src/components/Workspace/NetworkBrowserPanel/WorkspaceNamePanel.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   IconButton,
+  TextField,
   Theme,
   Tooltip,
   Typography,
@@ -9,6 +10,7 @@ import {
 import { useWorkspaceStore } from '../../../store/WorkspaceStore'
 import { Workspace } from '../../../models'
 import EditIcon from '@mui/icons-material/Edit'
+import { useState } from 'react'
 
 export const WorkspaceNamePanel = () => {
   const theme: Theme = useTheme()
@@ -17,50 +19,97 @@ export const WorkspaceNamePanel = () => {
   const borderColor = theme.palette.divider
 
   const workspace: Workspace = useWorkspaceStore((state) => state.workspace)
+  const setWorkspaceName = useWorkspaceStore((state) => state.setName)
+
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+
+  const handleNameChange = (event: any): void => {
+    setWorkspaceName(event.target.value)
+  }
+
+  const handleBlur = () => {
+    setIsEditing(false)
+  }
+
+  const handleKeyDown = (event: any): void => {
+    if (event.key === 'Escape' || event.key === 'Enter') {
+      setIsEditing(false)
+    }
+  }
+
   return (
     <Box
       sx={{
         width: '100%',
+        height: '3em',
         backgroundColor: background,
         color: textColor,
         p: theme.spacing(1),
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         borderBottom: `2px solid ${borderColor}`,
       }}
     >
       <Tooltip
-        title={`Created at ${workspace.creationTime.toLocaleString()}`}
-        placement="top"
+        title={`This workspace was created at ${workspace.creationTime.toLocaleString()}`}
+        placement="bottom"
+        sx={{ flexGrow: 1, width: '100%' }}
       >
         <Box
           sx={{
             display: 'flex',
             alignItems: 'center',
-            justifyContent: 'center',
+            justifyContent: 'flex-start',
+            width: '100%',
           }}
         >
-          <Typography variant="body2">Name:</Typography>
-          <Typography
-            sx={{ paddingLeft: '1em' }}
-            variant="subtitle1"
-            color={'info'}
-          >
-            {workspace.name}
-          </Typography>
+          <Typography variant="body1">Name:</Typography>
+          {isEditing ? (
+            <TextField
+              value={workspace.name}
+              onChange={handleNameChange}
+              onBlur={handleBlur}
+              onKeyDown={handleKeyDown}
+              autoFocus
+              variant="outlined"
+              size="small"
+              sx={{
+                p: 0,
+                width: '100%',
+                paddingLeft: '1em',
+              }}
+              inputProps={{
+                style: {
+                  color: textColor,
+                },
+              }}
+            />
+          ) : (
+            <Typography
+              sx={{ paddingLeft: '1em' }}
+              variant="subtitle1"
+              color={'info'}
+            >
+              {workspace.name}
+            </Typography>
+          )}
         </Box>
       </Tooltip>
-      <Tooltip title="Edit workspace properties">
-        <IconButton
-          size="small"
-          onClick={() => {
-            console.log('Edit workspace properties')
-          }}
-        >
-          <EditIcon sx={{ fontSize: '1em', color: textColor }} />
-        </IconButton>
-      </Tooltip>
+      {isEditing ? null : (
+        <Tooltip title="Edit workspace name">
+          <IconButton
+            size="small"
+            disabled={isEditing}
+            onClick={() => {
+              setIsEditing(true)
+            }}
+            sx={{ visibility: isEditing ? 'hidden' : 'visible' }}
+          >
+            <EditIcon sx={{ fontSize: '1em', color: textColor }} />
+          </IconButton>
+        </Tooltip>
+      )}
     </Box>
   )
 }
