846738513eaf5a290fec9a872ed3f630e3c724a4
Chengzhan Gao <1812535185@qq.com>
2024-10-02 16:52:29 -0700

    update on api and model

--- src/features/ServiceApps/model/index.ts
+++ src/features/ServiceApps/model/index.ts
@@ -1,15 +1,3 @@
-enum NodeType {
-  ARRAY = 'ARRAY',
-  BINARY = 'BINARY',
-  BOOLEAN = 'BOOLEAN',
-  MISSING = 'MISSING',
-  NULL = 'NULL',
-  NUMBER = 'NUMBER',
-  OBJECT = 'OBJECT',
-  POJO = 'POJO',
-  STRING = 'STRING',
-}
-
 enum ServerStatusType {
   ok = 'ok',
   error = 'error',
@@ -26,25 +14,6 @@ enum AlgorithmValidationType {
   string = 'string',
 }
 
-export interface ServiceAlgorithm {}
-
-export interface CustomParameter {
-  name: string
-  displayName: string
-  description: string
-  type: string
-  defaultValue: string
-  validationType: string
-  validationHelp: string
-  validationRegex: string
-  minValue: number
-  maxValue: number
-}
-
-export interface Algorithms {
-  algorithms: { [key: string]: Algorithm }
-}
-
 export interface ErrorResponse {
   errorCode: string
   message: string
@@ -54,105 +23,45 @@ export interface ErrorResponse {
   timestamp: string
 }
 
-export interface JsonNode {
-  float: boolean
-  array: boolean
-  empty: boolean
-  null: boolean
-  valueNode: boolean
-  containerNode: boolean
-  missingNode: boolean
-  object: boolean
-  nodeType: NodeType
-  pojo: boolean
-  number: boolean
-  integralNumber: boolean
-  floatingPointNumber: boolean
-  short: boolean
-  int: boolean
-  long: boolean
-  double: boolean
-  bigDecimal: boolean
-  bigInteger: boolean
-  textual: boolean
-  boolean: boolean
-  binary: boolean
+export interface ServiceAlgorithm {
+  name: string
+  parameters: AlgorithmParameter[]
+  version: string
+  action: string
+  rootMenu: string
+  description: string
+  selectedData: SelectedData
 }
 
-export interface Task {
-  id: string
-}
-
-export interface TaskRequest {
-  algorithm: string
-  data: JsonNode
-  customParameters?: { [key: string]: string }
-}
-
-export interface TaskStatus {
-  progress: number
-}
-
-export interface TaskResult {}
-
-export interface AlgorithmCustomParameter {
+export interface AlgorithmParameter {
   displayName: string
   description: string
   type: AlgorithmType
+  valueList: string[]
   defaultValue: string
   validationType: AlgorithmValidationType
   validationHelp: string
   validationRegex: string
   minValue: number
   maxValue: number
+  flag:string
 }
 
-export interface ServiceAlgorithm {
+export interface SelectedData {
+  type: string
+  parameters: SelectedDataParameter[]
+  scope: string
+}
+
+export interface SelectedDataParameter{
   name: string
-  displayName: string
+  format: string
   description: string
-  version: string
-  parameters: AlgorithmCustomParameter[]
+  dataType: string
+  model: string
 }
 
-export interface ServiceMetaData {
-  algorthms: ServiceAlgorithm[]
-  name: string
-  description: string
-  inputDataFormat: string
-  outputDataFormat: string
-}
-
-export interface ServerStatus {
-  status: ServerStatusType
-  pcDiskFull: number
-  load: [number, number, number]
-  queuedTasks: number
-  completedTasks: number
-  canceledTasks: number
-  restVersion: string
-}
-
-export interface CommunityDetectionAlgorithm extends ServiceAlgorithm {
-  name: string
-  displayName: string
-  description: string
-  version: string
-  dockerImage: string
-  inputDataFormat: string
-  outputDataFormat: string
-  rawResultContentType: string
-  binaryResult: boolean
-  customParameters: CustomParameter[]
-}
-
-export interface CommunityDetectionRequest extends TaskRequest {
-  algorithm: string
-  data: JsonNode
-  customParameters?: { [key: string]: string }
-}
-
-export interface CommunityDetectionResult extends TaskResult {
+export interface CytoContainerResult{
   id: string
   status: string
   message: string
@@ -162,11 +71,35 @@ export interface CommunityDetectionResult extends TaskResult {
   result: JsonNode
 }
 
-export interface CommunityDetectionResultStatus extends TaskStatus {
+export interface CytoContainerResultStatus{
   id: string
   status: string
   message: string
   progress: number
   wallTime: number
   startTime: number
+}
+
+export interface JsonNode {
+  [key: string]: any; 
+}
+
+export interface Task {
+  id: string
+}
+
+export interface CytoContainerRequest {
+  algorithm: string
+  data: JsonNode
+  customParameters?: { [key: string]: string }
+}
+
+export interface ServerStatus {
+  status: ServerStatusType
+  pcDiskFull: number
+  load: [number, number, number]
+  queuedTasks: number
+  completedTasks: number
+  canceledTasks: number
+  version: string
 }
