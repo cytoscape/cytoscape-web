f54bfad0fe9e2d217afcd2831cc29aa5e61ff379
Chengzhan Gao <1812535185@qq.com>
2024-10-03 15:49:35 -0700

    temp

--- src/features/ServiceApps/index.ts
+++ src/features/ServiceApps/index.ts
@@ -1,3 +1,4 @@
+import { Table, IdType } from '../../models'
 import { deleteTask, getTaskResult, getTaskStatus, submitTask } from './api'
 import {
   CytoContainerRequest,
@@ -9,18 +10,57 @@ import {
   Task,
 } from './model'
 
-const POLL_INTERVAL = 500
+const POLL_INTERVAL = 500 // 0.5 seconds
 
-export const assemblInputData = async (
+export const createDataObject = async (
+  table:Table,
   type: string,
   scope: string,
   inputColumns: InputColumn[],
-  inputNetwork: InputNetwork
 ) => {
   
   return
 }
 
+const serializeColumns = (table: Table,columns: InputColumn[]): JsonNode => {
+  const serializedColumns = columns.map((column) => {
+    return {
+      id: column.name,
+      type: column.dataType,
+    }
+  })
+
+  const rows = table.rows.map((row) => {
+    return 
+  })
+  return {
+    columns: {serializedColumns},
+    rows: {}
+  }
+}
+           
+function filterTable(
+  table: Table,
+  selectedNodeIds: IdType[],   // List of node IDs to filter
+  selectedColumns: AttributeName[] // List of columns to filter
+): Record<IdType,Record<AttributeName, ValueType>> {
+  // Step 1 & 2: Use reduce to filter both rows and columns
+  return selectedNodeIds.reduce((acc, nodeId) => {
+    const row = table.rows.get(nodeId);
+    if (row) {
+      // Filter the columns for the current row
+      const filteredRow = selectedColumns.reduce((colAcc, columnName) => {
+        if (row.hasOwnProperty(columnName)) {
+          colAcc[columnName] = row[columnName];
+        }
+        return colAcc;
+      }, {} as Partial<Record<AttributeName, ValueType>>);
+
+      acc[nodeId] = filteredRow;
+    }
+    return acc;
+  }, {} as Record<IdType, Partial<Record<AttributeName, ValueType>>>);
+}
 
 export const runTask = async (
   serviceUrl: string,
