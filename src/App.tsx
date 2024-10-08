d274fd083b71205d6aca3069fed6c5138f233a6a
yihangx <yihang.xin@gladstone.ucsf.edu>
2024-10-08 01:52:36 -0700

    implement cookie consent banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { Suspense, useContext, useEffect } from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
+import CookieConsent from 'react-cookie-consent';
+import Cookies from 'js-cookie';
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import './index.css'
 import { Error } from './Error'
@@ -82,19 +84,43 @@ const router = createBrowserRouter(
 )
 
 export const App = (): React.ReactElement => {
-  const client = useContext(KeycloakContext)
-  const setClient = useCredentialStore((state) => state.setClient)
+  const client = useContext(KeycloakContext);
+  const setClient = useCredentialStore((state) => state.setClient);
 
   useEffect(() => {
-    setClient(client)
-  }, [])
+    setClient(client);
+  }, [client, setClient]);
+
+  const removeAllCookies = () => {
+    const allCookies = Cookies.get();
+    Object.keys(allCookies).forEach(cookieName => {
+      Cookies.remove(cookieName, { path: '/' });
+    });
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <ErrorBoundary>
         <RouterProvider router={router} />
+        <CookieConsent
+          location="bottom"
+          buttonText="Accept"
+          declineButtonText="Decline"
+          enableDeclineButton
+          setDeclineCookie={false}
+          flipButtons 
+          onDecline={removeAllCookies}
+          cookieName="myAppCookieConsent"
+          style={{ background: "#4F4F4F" }}
+          buttonStyle={{ backgroundColor: "#0073B0", color: "#ffffff", fontSize: "13px" }}
+          declineButtonStyle={{ color: "#ffffff", background: "#6c757d", fontSize: "13px" }}
+          expires={150}
+        >
+          This site uses cookies to support Cytoscape Web’s network visualization tools and improve your experience. By accepting, you consent to our data practices.{" "}
+          <a href="https://github.com/cytoscape/cytoscape-web/blob/development/privacy-policy.md" style={{ color: "#e0e0e0" }}>Learn more</a>
+        </CookieConsent>
       </ErrorBoundary>
     </ThemeProvider>
-  )
-}
+  );
+};
