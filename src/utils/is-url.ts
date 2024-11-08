882f685df9f8a80513f5b7ea6addba5aa845aeee
Dylan Fong <dylanfong.ut@gmail.com>
2024-11-08 14:58:26 -0500

    Fix issue where certain strings would be rendered as clickable URLs

--- src/utils/is-url.ts
+++ src/utils/is-url.ts
@@ -1,8 +1,6 @@
 export function isValidUrl(input: string): boolean {
-  try {
-    const url = new URL(input)
-    return url != null
-  } catch (e) {
-    return false
-  }
+  const urlPattern =
+    /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[a-zA-Z0-9-._~:\/?#[\]@!$&'()*+,;=]*)?$/
+
+  return urlPattern.test(input)
 }
