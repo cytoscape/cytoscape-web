f7d0799d94530f28949bbc1b58f883004f39bca4
yihangx <yihang.xin@gladstone.ucsf.edu>
2024-12-04 19:42:52 -0800

    update user manual link

--- src/components/ToolBar/HelpMenu/TutorialMenuItem.tsx
+++ src/components/ToolBar/HelpMenu/TutorialMenuItem.tsx
@@ -4,13 +4,13 @@ import { BaseMenuProps } from '../BaseMenuProps'
 
 export const TutorialMenuItem = (props: BaseMenuProps): ReactElement => {
   const handleUserManual = (): void => {
-    window.open('https://github.com/cytoscape/cytoscape-web/wiki/Tutorial-For-Cytoscape-Web', '_blank')
+    window.open('https://web-manual.cytoscape.org', '_blank')
     props.handleClose()
   }
 
   return (
-    <MenuItem onClick={handleUserManual} disabled={true}>
-      User Manual (Coming soon)
+    <MenuItem onClick={handleUserManual} disabled={false}>
+      User Manual
     </MenuItem>
   )
 }
