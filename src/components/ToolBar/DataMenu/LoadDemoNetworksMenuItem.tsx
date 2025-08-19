591dbdbfd2d0bb69b0c80f6e383a372925b87d85
Dylan Fong <dylanfong.ut@gmail.com>
2025-08-19 11:50:00 -0400

    Fix issue where demo networks would not load properly

--- src/components/ToolBar/DataMenu/LoadDemoNetworksMenuItem.tsx
+++ src/components/ToolBar/DataMenu/LoadDemoNetworksMenuItem.tsx
@@ -4,6 +4,11 @@ import { AppConfigContext } from '../../../AppConfigContext'
 import { IdType } from '../../../models/IdType'
 import { useWorkspaceStore } from '../../../store/WorkspaceStore'
 import { BaseMenuProps } from '../BaseMenuProps'
+import { useUrlNavigation } from '../../../store/hooks/useUrlNavigation/useUrlNavigation'
+import { ndexSummaryFetcher } from '../../../store/getNetworkSummaryFromCacheOrNdex'
+import { useNetworkSummaryStore } from '../../../store/NetworkSummaryStore'
+import { useCredentialStore } from '../../../store/CredentialStore'
+import { NdexNetworkSummary } from '../../../models'
 
 export const LoadDemoNetworksMenuItem = (
   props: BaseMenuProps,
@@ -14,9 +19,38 @@ export const LoadDemoNetworksMenuItem = (
 
   const { testNetworks } = useContext(AppConfigContext)
 
-  const handleAddDemoNetworks = (): void => {
-    props.handleClose()
+  const { navigateToNetwork } = useUrlNavigation()
+
+  const workspace = useWorkspaceStore((state) => state.workspace)
+  const { ndexBaseUrl } = useContext(AppConfigContext)
+  const addSummaries = useNetworkSummaryStore((state) => state.addAll)
+  const setCurrentNetworkId = useWorkspaceStore(
+    (state) => state.setCurrentNetworkId,
+  )
+  const { getToken } = useCredentialStore()
+  const handleAddDemoNetworks = async () => {
+    const token = await getToken()
+    const summaries = await ndexSummaryFetcher(testNetworks, ndexBaseUrl, token)
     addNetworks(testNetworks)
+
+    addSummaries(
+      summaries.reduce(
+        (acc, summary) => {
+          acc[summary.externalId] = summary
+          return acc
+        },
+        {} as Record<IdType, NdexNetworkSummary>,
+      ),
+    )
+
+    setCurrentNetworkId(testNetworks[0])
+    navigateToNetwork({
+      workspaceId: workspace.id,
+      networkId: testNetworks[0],
+      searchParams: new URLSearchParams(location.search),
+      replace: false,
+    })
+    props.handleClose()
   }
 
   return (
