ce970140415f91725fa479aa669137bea46d4d1c
Max Franz <maxkfranz@gmail.com>
2026-06-16 12:07:58 -0400

    lower timout

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -5,4 +5,4 @@ import { vi } from 'vitest'
 // Enable Immer's MapSet plugin to support Map and Set in Immer
 enableMapSet()
 
-vi.setConfig({ testTimeout: 100000 })
+vi.setConfig({ testTimeout: 1000 })
